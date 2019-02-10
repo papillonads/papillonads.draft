ac83e46fdcc8de41aace8cbe5e980f3a7cde4158
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-10 20:12:00 +0100

    remove unused route references

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -4,18 +4,7 @@ import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import {
-  startPageRoute,
-  alertsPageRoute,
-  avatarsPageRoute,
-  blankslatePageRoute,
-  breadcrumbPageRoute,
-  buttonsPageRoute,
-  labelsPageRoute,
-  layoutPageRoute,
-  navigationPageRoute,
-  notFoundPageRoute,
-} from './route'
+import { startPageRoute, notFoundPageRoute } from './route'
 import './index.scss'
 import '@babel/polyfill'
 import client from './graphql/client'
@@ -27,14 +16,6 @@ const App = () => (
     <ApolloProvider client={client}>
       <Switch>
         <Route exact={startPageRoute.exact} path={startPageRoute.path} component={startPageRoute.component} />
-        <Route exact={alertsPageRoute.exact} path={alertsPageRoute.path} component={alertsPageRoute.component} />
-        <Route exact={avatarsPageRoute.exact} path={avatarsPageRoute.path} component={avatarsPageRoute.component} />
-        <Route exact={blankslatePageRoute.exact} path={blankslatePageRoute.path} component={blankslatePageRoute.component} />
-        <Route exact={breadcrumbPageRoute.exact} path={breadcrumbPageRoute.path} component={breadcrumbPageRoute.component} />
-        <Route exact={buttonsPageRoute.exact} path={buttonsPageRoute.path} component={buttonsPageRoute.component} />
-        <Route exact={labelsPageRoute.exact} path={labelsPageRoute.path} component={labelsPageRoute.component} />
-        <Route exact={layoutPageRoute.exact} path={layoutPageRoute.path} component={layoutPageRoute.component} />
-        <Route exact={navigationPageRoute.exact} path={navigationPageRoute.path} component={navigationPageRoute.component} />
         <Route component={notFoundPageRoute.component} />
       </Switch>
     </ApolloProvider>
