d263297d27969d42a9676d58f2fd30cb3e7262ec
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-10 20:07:29 +0100

    remove Header, SideBar and Intro

--- packages/client/src/pattern/template/StartTemplate/StartTemplate.js
+++ packages/client/src/pattern/template/StartTemplate/StartTemplate.js
@@ -1,24 +1,6 @@
 import React from 'react'
-import { version } from '../../../../../../lerna.json'
-import Intro from '../../molecule/banner/Intro'
-import Header from '../../molecule/navigation/Header'
-import SideBar from '../../molecule/navigation/SideBar'
+// import { version } from '../../../../../../lerna.json'
 
-const StartTemplate = () => (
-  <div>
-    <Header>Papillon Explorer v{version}</Header>
-    <div className="d-flex flex-wrap flex-lg-nowrap">
-      <SideBar />
-      <Intro>
-        <h1 className="f000-light mb-4">Papillon Explorer</h1>
-        <div className="f2-light pb-4">
-          {
-            "Resources, tooling, and design guidelines for building websites with Papillon, React components for GitHub's Primer Design System"
-          }
-        </div>
-      </Intro>
-    </div>
-  </div>
-)
+const StartTemplate = () => <div>Start</div>
 
 export default StartTemplate
