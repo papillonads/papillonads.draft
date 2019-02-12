19046f1b2397d2dd205f235296ac62b148e95593
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-12 20:06:47 +0100

    Updates

--- 1-modern.8448.js
+++ 1-modern.8448.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Ads Holland
- * @name           1-modern.e4ad.js
+ * @name           1-modern.8448.js
  * @author         MTS
- * @build          Tue, Feb 12, 2019 7:28 PM ET
- * @release        a985ebec5dc23228f1a691b7663280b6465f2890 [setup-ci-cd]
+ * @build          Tue, Feb 12, 2019 8:06 PM ET
+ * @release        46f39abf53d0bdd15dc363c85d927336455e0408 [master]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{157:function(e,a,t){"use strict";t.r(a);var n=t(0),r=t.n(n),p=t(44),s=t(77);var l=()=>r.a.createElement("div",null,"Start");a.default=(()=>r.a.createElement(p.Query,{query:s.a},e=>{let a=e.data;return a.pages&&a.pages.pages?r.a.createElement(l,{page:a.pages.pages.find(e=>"start"===e.id)}):null}))}}]);
-//# sourceMappingURL=1-modern.e4ad.js.map
+//# sourceMappingURL=1-modern.8448.js.map
