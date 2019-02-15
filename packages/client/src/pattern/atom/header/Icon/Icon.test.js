98413912876e513f258d790bef93646ed134ea85
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-15 15:28:36 +0100

    add Icon scaffolding

---
+++ packages/client/src/pattern/atom/header/Icon/Icon.test.js
@@ -0,0 +1,9 @@
+import defaultRender from './Icon.render'
+
+describe('<Icon />', () => {
+  describe('Snaphot', () => {
+    test('must match defaultRender', () => {
+      expect(global.renderToJSON(defaultRender)).toMatchSnapshot()
+    })
+  })
+})
