af53cfb53b10f922c6d4969d92c17d9fb9128175
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-15 12:27:04 +0100

    add NotFoundTemplate story

---
+++ packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.story.js
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/react'
+import { notFoundTemplateDefault } from './NotFoundTemplate.render'
+import { getInfo } from '../../../../../../.storybook/utility'
+
+const notFoundTemplateDefaultInfo = getInfo('NotFoundTemplate default')
+
+storiesOf('Patttern/Template/NotFoundTemplate', module).add(
+  notFoundTemplateDefaultInfo.text,
+  () => notFoundTemplateDefault,
+  notFoundTemplateDefaultInfo.parameters,
+)
