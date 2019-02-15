c158e26a0191af6c3683e5eac8c3da37ea07be4f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-15 16:04:19 +0100

    add Menu scaffolding

---
+++ packages/client/src/pattern/molecule/header/Menu/Menu.render.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Menu from './Menu'
+
+const defaultRender = <Menu />
+
+export { defaultRender }
---
+++ packages/client/src/pattern/molecule/header/Menu/Menu.test.js
@@ -0,0 +1,9 @@
+import defaultRender from './Menu.render'
+
+describe('<Menu />', () => {
+  describe('Snaphot', () => {
+    test('must match defaultRender', () => {
+      expect(global.renderToJSON(defaultRender)).toMatchSnapshot()
+    })
+  })
+})
