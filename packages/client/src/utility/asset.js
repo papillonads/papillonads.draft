bc756be1253a4e7591d7c5b88c920010f7623c62
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-16 17:35:48 +0100

    abstract icomoon and marktplaats selection.json

--- packages/client/src/utility/asset.js
+++ packages/client/src/utility/asset.js
@@ -1,9 +1,9 @@
 /* eslint no-console : 0 */
-// import iconPaths from '../../../../asset/icomoon-svg/selection.json'
-import iconPaths from '../../../../asset/icomoon-svg/selection.json'
+import iconPathsIcoMoon from '../../../../asset/icomoon-svg/selection-icomoon.json'
+import iconPathsMarktplaats from '../../../../asset/icomoon-svg/selection-marktplaats.json'
 
 export const getSvgIconPath = name => {
-  const icon = iconPaths.icons.find(icon => icon.properties.name === name)
+  const icon = iconPathsIcoMoon.icons.find(icon => icon.properties.name === name)
   if (icon) {
     return icon.icon.paths.join(' ')
   } else {
@@ -13,11 +13,15 @@ export const getSvgIconPath = name => {
 }
 
 export const getSvgIconGroupPaths = name => {
-  const icon = iconPaths.icons.find(icon => icon.properties.name === name)
+  const icon = iconPathsMarktplaats.icons.find(icon => icon.properties.name === name)
   if (icon) {
     return icon.icon.paths
   } else {
     console.warn(`svg icon group ${name} does not exist.`)
     return ''
   }
+}
+
+export const getFill = pathProps => {
+  return pathProps ? pathProps.fill : ''
 }
