5965ea268a066d5968e66722a63431d793b4210d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-16 17:16:15 +0100

    add getSvgIconGroupPaths()

--- packages/client/src/utility/asset.js
+++ packages/client/src/utility/asset.js
@@ -7,7 +7,17 @@ export const getSvgIconPath = name => {
   if (icon) {
     return icon.icon.paths.join(' ')
   } else {
-    console.warn(`icon ${name} does not exist.`)
+    console.warn(`svg icon ${name} does not exist.`)
+    return ''
+  }
+}
+
+export const getSvgIconGroupPaths = name => {
+  const icon = iconPaths.icons.find(icon => icon.properties.name === name)
+  if (icon) {
+    return icon.icon.paths
+  } else {
+    console.warn(`svg icon group ${name} does not exist.`)
     return ''
   }
 }
