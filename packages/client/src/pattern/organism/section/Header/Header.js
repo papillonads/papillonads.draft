68c446a2f8a634b64b5ddcb5fbf10757e45adb25
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 22:37:39 +0100

    Header style scaffolding

--- packages/client/src/pattern/organism/section/Header/Header.js
+++ packages/client/src/pattern/organism/section/Header/Header.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import { shape, string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Header.scss'
+import NavigationBar from '../../../molecule/header/NavigationBar/NavigationBar'
+import SearchBar from '../../../molecule/header/SearchBar/SearchBar'
 
-const Header = () => <div />
+const Header = ({ className }) => {
+  const headerClassList = cn(className, styles.header)
+
+  return (
+    <div className={headerClassList}>
+      <NavigationBar className={styles.navigationBar} />
+      <SearchBar className={styles.searchBar} />
+    </div>
+  )
+}
+
+Header.defaultProps = {
+  className: '',
+}
+
+Header.propTypes = shape({
+  className: string,
+}).isRequired
 
 export default Header
