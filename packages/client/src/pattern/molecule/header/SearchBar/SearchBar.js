4374fff8103e3919bb739af18830075ec6929f22
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 22:37:05 +0100

    SearchBar style scaffolding

--- packages/client/src/pattern/molecule/header/SearchBar/SearchBar.js
+++ packages/client/src/pattern/molecule/header/SearchBar/SearchBar.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { shape, string } from 'prop-types'
+import cn from 'classnames'
+import styles from './SearchBar.scss'
 
-const SearchBar = () => <div />
+const SearchBar = ({ className }) => {
+  const searchBarClassList = cn(className, styles.searchBar)
+
+  return <div className={searchBarClassList}>SearchBar</div>
+}
+
+SearchBar.defaultProps = {
+  className: '',
+}
+
+SearchBar.propTypes = shape({
+  className: string,
+}).isRequired
 
 export default SearchBar
