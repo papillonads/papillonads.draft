3a74d515788126a11c4b32001f408368ed390bf8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 22:36:40 +0100

    NavigationBar style scaffolding

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.js
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { shape, string } from 'prop-types'
+import cn from 'classnames'
+import styles from './NavigationBar.scss'
 
-const NavigationBar = () => <div />
+const NavigationBar = ({ className }) => {
+  const navigationBarClassList = cn(className, styles.navigationBar)
+
+  return <div className={navigationBarClassList}>NavigationBar</div>
+}
+
+NavigationBar.defaultProps = {
+  className: '',
+}
+
+NavigationBar.propTypes = shape({
+  className: string,
+}).isRequired
 
 export default NavigationBar
