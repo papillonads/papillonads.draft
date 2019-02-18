0225e95216e7c041e336c1e75930c3cb8771bcd7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 23:05:35 +0100

    add Logo scaffolding

--- packages/client/src/pattern/atom/header/Logo/Logo.js
+++ packages/client/src/pattern/atom/header/Logo/Logo.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { shape, string } from 'prop-types'
+import cn from 'classnames'
+import styles from './logo.scss'
 
-const Logo = () => <div />
+const Logo = ({ className }) => {
+  const logoClassList = cn(className, styles.logo)
+
+  return <div className={logoClassList}>Logo</div>
+}
+
+Logo.defaultProps = {
+  className: '',
+}
+
+Logo.propTypes = shape({
+  className: string,
+}).isRequired
 
 export default Logo
