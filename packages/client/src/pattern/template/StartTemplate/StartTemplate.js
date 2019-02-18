49ea9ba684d9ce647bf0371aafd13d02c72ca8a1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 21:48:26 +0100

    add Header

--- packages/client/src/pattern/template/StartTemplate/StartTemplate.js
+++ packages/client/src/pattern/template/StartTemplate/StartTemplate.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import Header from '../../organism/section/Header/Header'
 
-const StartTemplate = () => <div>Start</div>
+const StartTemplate = () => <Header />
 
 export default StartTemplate
