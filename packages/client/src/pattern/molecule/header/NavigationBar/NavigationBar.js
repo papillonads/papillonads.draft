62d99133b1af6b09b2ef89a817a2d5204ae4399c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 23:07:08 +0100

    extend NavigationBar with Logo and Links

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.js
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { shape, string } from 'prop-types'
 import cn from 'classnames'
 import styles from './NavigationBar.scss'
+import Logo from '../../../atom/header/Logo'
+import Links from '../Links'
 
 const NavigationBar = ({ className }) => {
   const navigationBarClassList = cn(className, styles.navigationBar)
 
-  return <div className={navigationBarClassList}>NavigationBar</div>
+  return (
+    <div className={navigationBarClassList}>
+      <Logo className={styles.logo} />
+      <Links className={styles.links} />
+    </div>
+  )
 }
 
 NavigationBar.defaultProps = {
