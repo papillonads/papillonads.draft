3727d0d7c5d888d0b05a607711449d7bcc4b3cbb
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-18 23:05:09 +0100

    add Links style scaffolding

--- packages/client/src/pattern/molecule/header/Links/Links.js
+++ packages/client/src/pattern/molecule/header/Links/Links.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { shape, string } from 'prop-types'
+import cn from 'classnames'
+import styles from './links.scss'
 
-const Links = () => <div />
+const Links = ({ className }) => {
+  const linksClassList = cn(className, styles.links)
+
+  return <div className={linksClassList}>Links</div>
+}
+
+Links.defaultProps = {
+  className: '',
+}
+
+Links.propTypes = shape({
+  className: string,
+}).isRequired
 
 export default Links
