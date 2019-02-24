64ed986d30db0331e6f4778b4dd969d8206c3311
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 09:56:00 +0100

    add Button style scaffolding

--- packages/client/src/pattern/atom/header/Button/Button.js
+++ packages/client/src/pattern/atom/header/Button/Button.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Button.scss'
 
-const Button = () => <div />
+const Button = ({ className }) => {
+  const buttonClassList = cn(className, styles.button)
+
+  return <div className={buttonClassList}>Button</div>
+}
+
+Button.defaultProps = {
+  className: '',
+}
+
+Button.propTypes = {
+  className: string,
+}.isRequired
 
 export default Button
