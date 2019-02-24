839cc98fb04f6e728193739356329051f9f71efe
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 14:43:47 +0100

    add text prop

--- packages/client/src/pattern/atom/header/Label/Label.jsx
+++ packages/client/src/pattern/atom/header/Label/Label.jsx
@@ -3,10 +3,10 @@ import { string } from 'prop-types'
 import cn from 'classnames'
 import styles from './Label.scss'
 
-const Label = ({ className }) => {
+const Label = ({ className, text }) => {
   const labelClassList = cn(className, styles.label)
 
-  return <div className={labelClassList}>Label</div>
+  return <div className={labelClassList}>{text}</div>
 }
 
 Label.defaultProps = {
--- packages/client/src/pattern/atom/header/Label/Label.render.js
+++ packages/client/src/pattern/atom/header/Label/Label.render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Label from './Label'
 
-const defaultRender = <Label />
+const defaultRender = <Label text="some-text" />
 
 export { defaultRender }
