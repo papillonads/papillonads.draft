741cb0f251decef57f6224e623b1d17a68d0523b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 11:20:58 +0100

    add NotFoundPage style scaffolding

--- packages/client/src/pattern/page/NotFoundPage/NotFoundPage.js
+++ packages/client/src/pattern/page/NotFoundPage/NotFoundPage.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import { object } from 'prop-types'
+import { string, object } from 'prop-types'
+import cn from 'classnames'
+import styles from './Logo.scss'
 import { Query } from 'react-apollo'
 import { PAGES } from '../../../graphql/query'
 import NotFoundTemplate from '../../template/NotFoundTemplate'
 
-const NotFoundPage = ({ location }) => {
+const NotFoundPage = ({ className, location }) => {
+  const notFoundPageClassList = cn(className, styles.notFoundPage)
+
   return (
-    <Query query={PAGES}>
-      {({ data }) =>
-        data.pages && data.pages.pages ? (
-          <NotFoundTemplate page={data.pages.pages.find(page => page.id === 'not-found')} location={location} />
-        ) : null
-      }
-    </Query>
+    <div className={notFoundPageClassList}>
+      <Query query={PAGES}>
+        {({ data }) =>
+          data.pages && data.pages.pages ? (
+            <NotFoundTemplate page={data.pages.pages.find(page => page.id === 'not-found')} location={location} />
+          ) : null
+        }
+      </Query>
+    </div>
   )
 }
 
+NotFoundPage.defaultProps = {
+  className: '',
+}
+
 NotFoundPage.propTypes = {
+  className: string,
   location: object.isRequired,
 }
 
