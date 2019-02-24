21c88f5ea72b1fdc56d83c608b52aa0d556e4102
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 10:59:27 +0100

    add Dropdown style scaffolding

--- packages/client/src/pattern/molecule/header/Dropdown/Dropdown.js
+++ packages/client/src/pattern/molecule/header/Dropdown/Dropdown.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Dropdown.scss'
 
-const Dropdown = () => <div />
+const Dropdown = ({ className }) => {
+  const dropdownClassList = cn(className, styles.dropdown)
+
+  return <div className={dropdownClassList}>Dropdown</div>
+}
+
+Dropdown.defaultProps = {
+  className: '',
+}
+
+Dropdown.propTypes = {
+  className: string,
+}.isRequired
 
 export default Dropdown
