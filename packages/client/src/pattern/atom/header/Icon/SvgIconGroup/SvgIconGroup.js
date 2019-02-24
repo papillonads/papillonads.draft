3fc6d60c0256a5b17848f29e0664e45894baec36
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 10:01:56 +0100

    add SVGIconGroup style scaffolding

--- packages/client/src/pattern/atom/header/Icon/SvgIconGroup/SvgIconGroup.js
+++ packages/client/src/pattern/atom/header/Icon/SvgIconGroup/SvgIconGroup.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { svgIconGroupShape } from '../SvgIcon.shape'
+import cn from 'classnames'
+import styles from './SvgIconGroup.scss'
 import { getSvgIconGroupPaths, getFill } from '../../../../../utility/asset'
 import uuidv1 from 'uuid/v1'
 
 const SvgIconGroup = ({ className, name, width, height, viewBox, fill, pathProps }) => {
+  const svgIconGroupClassList = cn(className, styles.svgIconGroup)
   const paths = getSvgIconGroupPaths(name)
 
   if (!(paths && paths.length > 0)) {
@@ -11,7 +14,7 @@ const SvgIconGroup = ({ className, name, width, height, viewBox, fill, pathProps
   }
 
   return (
-    <svg className={className} width={width} height={height} viewBox={viewBox}>
+    <svg className={svgIconGroupClassList} width={width} height={height} viewBox={viewBox}>
       <g fill={fill} fillRule="evenodd">
         {paths.map((path, index) => {
           return <path key={uuidv1()} d={paths[index]} fill={getFill(pathProps[index])} />
