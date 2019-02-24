e4633b8127f16865e1307159f6fab5a9b527f12f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 11:52:01 +0100

    add StartPage style scaffolding

--- packages/client/src/pattern/page/StartPage/StartPage.js
+++ packages/client/src/pattern/page/StartPage/StartPage.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './StartPage.scss'
 import { Query } from 'react-apollo'
 import { PAGES } from '../../../graphql/query'
 import StartTemplate from '../../template/StartTemplate'
 
-const StartPage = () => {
+const StartPage = ({ className }) => {
+  const startPageClassList = cn(className, styles.startPage)
+
   return (
-    <Query query={PAGES}>
-      {({ data }) => (data.pages && data.pages.pages ? <StartTemplate page={data.pages.pages.find(page => page.id === 'start')} /> : null)}
-    </Query>
+    <div className={startPageClassList}>
+      <Query query={PAGES}>
+        {({ data }) =>
+          data.pages && data.pages.pages ? <StartTemplate page={data.pages.pages.find(page => page.id === 'start')} /> : null
+        }
+      </Query>
+    </div>
   )
+}
+
+StartPage.defaultProps = {
+  className: '',
+}
+
+StartPage.propTypes = {
+  className: string,
 }
 
 export default StartPage
