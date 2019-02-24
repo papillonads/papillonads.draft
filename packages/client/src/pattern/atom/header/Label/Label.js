fd04362277de525809659b167adab79a90ba7ca7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 10:26:33 +0100

    add Label style scaffolding

--- packages/client/src/pattern/atom/header/Label/Label.js
+++ packages/client/src/pattern/atom/header/Label/Label.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Label.scss'
 
-const Label = () => <div />
+const Label = ({ className }) => {
+  const labelClassList = cn(className, styles.label)
+
+  return <div className={labelClassList}>Label</div>
+}
+
+Label.defaultProps = {
+  className: '',
+}
+
+Label.propTypes = {
+  className: string,
+}.isRequired
 
 export default Label
