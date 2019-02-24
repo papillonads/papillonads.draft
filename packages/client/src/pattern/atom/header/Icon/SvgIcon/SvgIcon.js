27ca77e0b5042d91dcdf9bc2a329d91b7e6e5316
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 09:56:35 +0100

    add SvgIcon style scaffolding

--- packages/client/src/pattern/atom/header/Icon/SvgIcon/SvgIcon.js
+++ packages/client/src/pattern/atom/header/Icon/SvgIcon/SvgIcon.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { svgIconShape } from '../SvgIcon.shape'
+import cn from 'classnames'
+import styles from './Logo.scss'
 import { getSvgIconPath } from '../../../../../utility/asset'
 
-const SvgIcon = ({ className, name, width, height, fill }) => (
-  <svg fill={fill} width={width} height={height} className={className} viewBox="0 0 1024 1024">
-    <path d={getSvgIconPath(name)} />
-  </svg>
-)
+const SvgIcon = ({ className, name, width, height, fill }) => {
+  const svgIconClassList = cn(className, styles.svgIcon)
+
+  return (
+    <svg fill={fill} width={width} height={height} className={svgIconClassList} viewBox="0 0 1024 1024">
+      <path d={getSvgIconPath(name)} />
+    </svg>
+  )
+}
 
 SvgIcon.defaultProps = {
   className: '',
