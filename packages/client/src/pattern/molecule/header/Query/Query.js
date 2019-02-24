958c0447955c3ca6b2918f87c0afe4fe331e652b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 11:13:04 +0100

    add Query style scaffolding

--- packages/client/src/pattern/molecule/header/Query/Query.js
+++ packages/client/src/pattern/molecule/header/Query/Query.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Query.scss'
 
-const Query = () => <div />
+const Query = ({ className }) => {
+  const queryClassList = cn(className, styles.query)
+
+  return <div className={queryClassList}>Query</div>
+}
+
+Query.defaultProps = {
+  className: '',
+}
+
+Query.propTypes = {
+  className: string,
+}.isRequired
 
 export default Query
