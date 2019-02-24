42f47f0372fefa22a70f5edb8b5bc2ba7e6e6376
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 12:01:14 +0100

    add NotFoundTemplate style scaffolding

--- packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.js
+++ packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { object } from 'prop-types'
+import { string, object } from 'prop-types'
+import cn from 'classnames'
+import styles from './NotFoundTemplate.scss'
 import { Link } from 'react-router-dom'
 import { startPagePath } from '../../../route/path'
 import NoMatch from '../../atom/info/NoMatch'
 
-const NotFoundTemplate = ({ page, location }) => (
-  <div>
-    <h2>{page.name}</h2>
-    <NoMatch location={location} />
-    <Link to={startPagePath}> Back to Home Page</Link>
-  </div>
-)
+const NotFoundTemplate = ({ className, page, location }) => {
+  const notFoundTemplateClassList = cn(className, styles.notFoundTemplate)
+
+  return (
+    <div className={notFoundTemplateClassList}>
+      <h2>{page.name}</h2>
+      <NoMatch location={location} />
+      <Link to={startPagePath}> Back to Home Page</Link>
+    </div>
+  )
+}
+
+NotFoundTemplate.defaultProps = {
+  className: '',
+}
 
 NotFoundTemplate.propTypes = {
+  className: string,
   page: object.isRequired,
   location: object.isRequired,
 }
