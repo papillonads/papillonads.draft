89c9ba8d831b52463cd4ba05cb4f9773a550eb46
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 10:55:30 +0100

    add UnderConstruction style scaffolding

--- packages/client/src/pattern/atom/info/UnderConstruction/UnderConstruction.js
+++ packages/client/src/pattern/atom/info/UnderConstruction/UnderConstruction.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './UnderConstruction.scss'
 
-const UnderConstruction = () => (
-  <React.Fragment>
-    Under construction{' '}
-    <span role="img" aria-label="emoji">
-      🙂
-    </span>
-  </React.Fragment>
-)
+const UnderConstruction = ({ className }) => {
+  const underConstructionClassList = cn(className, styles.underConstruction)
+
+  return (
+    <div className={underConstructionClassList}>
+      Under construction{' '}
+      <span role="img" aria-label="emoji">
+        🙂
+      </span>
+    </div>
+  )
+}
+
+UnderConstruction.defaultProps = {
+  className: '',
+}
+
+UnderConstruction.propTypes = {
+  className: string,
+}.isRequired
 
 export default UnderConstruction
