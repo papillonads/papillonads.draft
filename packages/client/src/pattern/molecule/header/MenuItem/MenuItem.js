82494f0b8099b34918590c63bf4f2abc448c3f51
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 11:10:46 +0100

    add MenuItem style scaffolding

--- packages/client/src/pattern/molecule/header/MenuItem/MenuItem.js
+++ packages/client/src/pattern/molecule/header/MenuItem/MenuItem.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './MenuItem.scss'
 
-const MenuItem = () => <div />
+const MenuItem = ({ className }) => {
+  const menuItemClassList = cn(className, styles.menuItem)
+
+  return <div className={menuItemClassList}>MenuItem</div>
+}
+
+MenuItem.defaultProps = {
+  className: '',
+}
+
+MenuItem.propTypes = {
+  className: string,
+}.isRequired
 
 export default MenuItem
