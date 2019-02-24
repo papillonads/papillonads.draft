ab362605269b31f74c95a3961427d0635008f9dc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-24 11:07:26 +0100

    add Menu style scffolding

--- packages/client/src/pattern/molecule/header/Menu/Menu.js
+++ packages/client/src/pattern/molecule/header/Menu/Menu.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { string } from 'prop-types'
+import cn from 'classnames'
+import styles from './Menu.scss'
 
-const Menu = () => <div />
+const Menu = ({ className }) => {
+  const menuClassList = cn(className, styles.menu)
+
+  return <div className={menuClassList}>Menu</div>
+}
+
+Menu.defaultProps = {
+  className: '',
+}
+
+Menu.propTypes = {
+  className: string,
+}.isRequired
 
 export default Menu
