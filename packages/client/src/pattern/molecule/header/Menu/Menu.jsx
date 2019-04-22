abfa0ae968a2d263970fc1e34b5529005d4cab19
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-22 17:40:49 +0200

    style Menu

--- packages/client/src/pattern/molecule/header/Menu/Menu.jsx
+++ packages/client/src/pattern/molecule/header/Menu/Menu.jsx
@@ -19,16 +19,16 @@ const Menu = ({ className }) => {
   //   { text: 'Uitloggen' },
   // ]
 
-  const menuClassList = cn(className, ps['d-flex'], ps['px-6'], ps['flex-justify-between'], cs.menu)
+  const menuClassList = cn(className, ps['d-flex'], ps['px-6'], ps['flex-justify-between'], cs._menu)
 
   return (
     <div className={menuClassList}>
-      <Badge className={ps['d-flex']}>
-        <SvgIcon className={ps['px-2']} name={'bubbles2'} width={15} height={15} />
+      <Badge className={cn(ps['d-flex'], cs._menu__label)}>
+        <SvgIcon className={ps['px-2']} fill={'grey'} name={'bubbles4'} width={15} height={15} />
         <Label className={ps['px-2']} text="Berichten" />
       </Badge>
-      <Badge className={ps['d-flex']}>
-        <SvgIcon className={ps['px-2']} name={'bell'} width={15} height={15} />
+      <Badge className={cn(ps['d-flex'], cs._menu__label)}>
+        <SvgIcon className={ps['px-2']} fill={'grey'} name={'bell'} width={15} height={15} />
         <Label className={ps['px-2']} text="Meldingen" />
       </Badge>
       {/* <Dropdown items={dropdownItems} /> */}
