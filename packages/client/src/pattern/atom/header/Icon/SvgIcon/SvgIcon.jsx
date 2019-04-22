fe330e80d0f070aa9739e72279ed0b5d9940b45c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-22 15:52:27 +0200

    style div

--- packages/client/src/pattern/atom/header/Icon/SvgIcon/SvgIcon.jsx
+++ packages/client/src/pattern/atom/header/Icon/SvgIcon/SvgIcon.jsx
@@ -8,9 +8,11 @@ const SvgIcon = ({ className, name, width, height, fill }) => {
   const svgIconClassList = cn(className, styles.svgIcon)
 
   return (
-    <svg fill={fill} width={width} height={height} className={svgIconClassList} viewBox="0 0 1024 1024">
-      <path d={getSvgIconPath(name)} />
-    </svg>
+    <div className={svgIconClassList}>
+      <svg fill={fill} width={width} height={height} viewBox="0 0 1024 1024">
+        <path d={getSvgIconPath(name)} />
+      </svg>
+    </div>
   )
 }
 
