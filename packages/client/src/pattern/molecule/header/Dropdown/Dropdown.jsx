cf298620efa01ede1664190b4af396998634674c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-22 21:39:14 +0200

    style Dropdown

--- packages/client/src/pattern/molecule/header/Dropdown/Dropdown.jsx
+++ packages/client/src/pattern/molecule/header/Dropdown/Dropdown.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import { shape, arrayOf, string } from 'prop-types'
 import cn from 'classnames'
-import styles from './Dropdown.scss'
+import ps from '@primer/css/core/index.scss'
+import cs from './Dropdown.scss'
 import Label from '../../../atom/header/Label'
+import { SvgIcon } from '../../../atom/header/Icon'
 import uuidv1 from 'uuid/v1'
+import Badge from '../Badge'
 
 const Dropdown = ({ className, items }) => {
-  const dropdownClassList = cn(className, styles.dropdown)
+  const dropdownClassList = cn(className, ps['dropdown'], ps['details-reset'], cs.dropdown)
 
   return (
-    <div className={dropdownClassList}>
-      {items.map(item => (
-        <Label key={uuidv1()} text={item.text} />
-      ))}
-    </div>
+    <details className={dropdownClassList}>
+      <summary aria-haspopup="true" className={cs.dropdown__summary}>
+        <Badge className={cn(ps['d-flex'], ps['p-2'], cs.dropdown__label)}>
+          <SvgIcon className={ps['px-1']} fill={'grey'} name={'user'} width={15} height={15} />
+          <Label className={ps['px-1']} text="t" />
+          <SvgIcon className={ps['px-1']} fill={'grey'} name={'ctrl'} width={15} height={15} />
+        </Badge>
+      </summary>
+
+      <ul className={cn(ps['dropdown-menu'], cs.dropdown__list)}>
+        {items.map(item => (
+          <li key={uuidv1()}>
+            <Label className={ps['dropdown-item']} key={uuidv1()} text={item.text} />
+          </li>
+        ))}
+      </ul>
+    </details>
   )
 }
 
