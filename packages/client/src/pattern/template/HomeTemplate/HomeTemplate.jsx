6f6364de52fb91d4a246ea6543f86289bdd91e45
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-22 17:38:41 +0200

    extend HomeTemplate with Ribbon

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
-import styles from './HomeTemplate.scss'
+import ps from '@primer/css/core/index.scss'
+import cs from './HomeTemplate.scss'
+import Ribbon from '../../atom/header/Ribbon'
 import Header from '../../organism/section/Header/Header'
 
 const HomeTemplate = ({ className }) => {
-  const homeTemplateClassList = cn(className, styles.homeTemplate)
+  const homeTemplateClassList = cn(className, cs.homeTemplate)
 
   return (
     <div className={homeTemplateClassList}>
-      <Header />
+      <Ribbon />
+      <div className={cn(ps['col-8'], ps['mx-auto'])}>
+        <Header />
+      </div>
     </div>
   )
 }
