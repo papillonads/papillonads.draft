d4fa3dafab4dd34271d66b144d493ada1fbac4cc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-25 19:26:02 +0200

    pad SearchFormOptions

--- packages/client/src/pattern/molecule/header/SearchFormOptions/SearchFormOptions.jsx
+++ packages/client/src/pattern/molecule/header/SearchFormOptions/SearchFormOptions.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
+import ps from '@primer/css/core/index.scss'
 import cs from './SearchFormOptions.scss'
 
 const SearchFormOptions = ({ className }) => {
-  const searchFormOptionsClassList = cn(className, cs.searchFormOptions)
+  const searchFormOptionsClassList = cn(className, ps['p-2'], cs.searchFormOptions)
 
   return <div className={searchFormOptionsClassList}>SearchFormOptions</div>
 }
