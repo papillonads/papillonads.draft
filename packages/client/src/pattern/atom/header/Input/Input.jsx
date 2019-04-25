c6fd610c81036fb390cec533ad0fb2e80297a85c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-25 21:03:40 +0200

    add and style placeholder

--- packages/client/src/pattern/atom/header/Input/Input.jsx
+++ packages/client/src/pattern/atom/header/Input/Input.jsx
@@ -3,18 +3,20 @@ import { string } from 'prop-types'
 import cn from 'classnames'
 import styles from './Input.scss'
 
-const Input = ({ className }) => {
+const Input = ({ className, placeholder }) => {
   const inputClassList = cn(className, styles.input)
 
-  return <input className={inputClassList} />
+  return <input className={inputClassList} placeholder={placeholder} />
 }
 
 Input.defaultProps = {
   className: '',
+  placeholder: '',
 }
 
 Input.propTypes = {
   className: string,
+  placeholder: string,
 }.isRequired
 
 export default Input
