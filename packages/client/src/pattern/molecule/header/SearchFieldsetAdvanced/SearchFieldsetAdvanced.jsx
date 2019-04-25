b074d43b188ae0b43ec66f1a92c43e68c04683f1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-25 21:06:18 +0200

    SearchFieldsetAdvanced scaffolding and styling

--- packages/client/src/pattern/molecule/header/SearchFieldsetAdvanced/SearchFieldsetAdvanced.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetAdvanced/SearchFieldsetAdvanced.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
 import cs from './SearchFieldsetAdvanced.scss'
+import Select from '../../../atom/header/Select'
+import Input from '../../../atom/header/Input'
+import { categories, distances } from './data'
 
 const SearchFieldsetAdvanced = ({ className }) => {
   const searchFieldsetAdvancedClassList = cn(className, cs.searchFieldsetAdvanced)
 
-  return <div className={searchFieldsetAdvancedClassList}>SearchFieldsetAdvanced</div>
+  return (
+    <div className={searchFieldsetAdvancedClassList}>
+      <Select className={cs.searchFieldsetAdvanced__group} items={categories} placeholder="Alle groepen..." />
+      <Input className={cs.searchFieldsetAdvanced__postcode} placeholder="Postcode" />
+      <Select className={cs.searchFieldsetAdvanced__distance} items={distances} placeholder="Alle afstanden..." />
+    </div>
+  )
 }
 
 SearchFieldsetAdvanced.defaultProps = {
