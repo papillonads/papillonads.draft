196dde65a0f57135fc8e992884185b1ef5755ecd
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-25 18:28:27 +0200

    SearchBar scaffolding

--- packages/client/src/pattern/molecule/header/SearchBar/SearchBar.jsx
+++ packages/client/src/pattern/molecule/header/SearchBar/SearchBar.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
 import cs from './SearchBar.scss'
+import SearchFieldsetStandard from '../SearchFieldsetStandard'
+import SearchFieldsetAdvanced from '../SearchFieldsetAdvanced'
+import Button from '../../../atom/header/Button'
+import SearchFormOptions from '../SearchFormOptions'
 
 const SearchBar = ({ className }) => {
   const searchBarClassList = cn(className, cs.searchBar)
 
-  return <div className={searchBarClassList}>SearchBar</div>
+  return (
+    <div className={searchBarClassList}>
+      <SearchFieldsetStandard />
+      <SearchFieldsetAdvanced />
+      <Button>Zoek</Button>
+      <SearchFormOptions />
+    </div>
+  )
 }
 
 SearchBar.defaultProps = {
