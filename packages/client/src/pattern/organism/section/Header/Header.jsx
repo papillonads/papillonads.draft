c6564728caf58be6a237b76181cff4215adb1892
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-25 15:57:13 +0200

    move grid from HomeTemplate to Header

--- packages/client/src/pattern/organism/section/Header/Header.jsx
+++ packages/client/src/pattern/organism/section/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
+import ps from '@primer/css/core/index.scss'
 import cs from './Header.scss'
 import NavigationBar from '../../../molecule/header/NavigationBar'
 // import SearchBar from '../../../molecule/header/SearchBar'
 
 const Header = ({ className }) => {
-  const headerClassList = cn(className, cs.header)
+  const headerClassList = cn(className, ps['mx-auto'], cs.header)
 
   return (
     <div className={headerClassList}>
-      <NavigationBar className={cs.navigationBar} />
-      {/*<SearchBar className={cs.searchBar} />*/}
+      <NavigationBar className={cs.header__navigationBar} />
+      {/*<SearchBar className={cs.header__searchBar} />*/}
     </div>
   )
 }
--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
-import ps from '@primer/css/core/index.scss'
 import cs from './HomeTemplate.scss'
 import Ribbon from '../../atom/header/Ribbon'
 import Header from '../../organism/section/Header/Header'
@@ -12,9 +11,7 @@ const HomeTemplate = ({ className }) => {
   return (
     <div className={homeTemplateClassList}>
       <Ribbon />
-      <div className={cn(ps['col-8'], ps['mx-auto'])}>
-        <Header />
-      </div>
+      <Header />
     </div>
   )
 }
