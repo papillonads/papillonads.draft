dcc6bad37360c077ab60432d087129bd24fdb8c5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-28 17:21:51 +0200

    remove primer from NavigationBar

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.jsx
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
-import ps from '@primer/css/core/index.scss'
 import cs from './NavigationBar.scss'
 import Logo from '../../../atom/header/Logo'
 import Links from '../Links'
@@ -10,15 +9,15 @@ import Menu from '../Menu'
 const NavigationBar = ({ className }) => {
   const linkItems = [{ text: 'Help en info' }, { text: 'Voorwaarden' }, { text: 'Veilig handelen' }]
 
-  const navigationBarClassList = cn(className, ps['d-flex'], ps['flex-justify-between'], ps['p-1'], cs.navigationBar)
+  const navigationBarClassList = cn(className, cs.navigationBar)
 
   return (
     <div className={navigationBarClassList}>
-      <div className={cn(ps['d-flex'])}>
-        <Logo className={cn(ps['pr-4'], cs.navigationBar__logo)} />
-        <Links className={cn(ps['pt-1'], cs.navigationBar__links)} items={linkItems} />
+      <div className={cn(cs.navigationBar__left)}>
+        <Logo className={cn(cs.navigationBar__leftLogo)} />
+        <Links className={cn(cs.navigationBar__leftLinks)} items={linkItems} />
       </div>
-      <Menu className={cn(ps['pt-1'], cs.navigationBar__menu)} />
+      <Menu className={cn(cs.navigationBar__rightMenu)} />
     </div>
   )
 }
