0567068a9c2cda98bc90d15a086278c6deab6f47
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-29 15:01:17 +0200

    use react-select with hooks

--- packages/client/src/pattern/molecule/header/SearchFieldsetAdvanced/SearchFieldsetAdvanced.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetAdvanced/SearchFieldsetAdvanced.jsx
@@ -2,18 +2,37 @@ import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
 import cs from './SearchFieldsetAdvanced.scss'
-import Select from '../../../atom/header/Select'
+import Select from 'react-select'
 import Input from '../../../atom/header/Input'
 import { categories, distances } from './data'
 
 const SearchFieldsetAdvanced = ({ className }) => {
   const searchFieldsetAdvancedClassList = cn(className, cs.searchFieldsetAdvanced)
 
+  /* eslint-disable */
+  const [category, setCategory] = React.useState('')
+  const [distance, setDistance] = React.useState('')
+  /* eslint-enable */
+
   return (
     <div className={searchFieldsetAdvancedClassList}>
-      <Select className={cs.searchFieldsetAdvanced__group} items={categories} placeholder="Alle groepen..." />
+      <Select
+        className={cs.searchFieldsetAdvanced__group}
+        options={categories}
+        onChange={selectedOption => {
+          setCategory(selectedOption.value)
+        }}
+        placeholder="Alle groepen..."
+      />
       <Input className={cs.searchFieldsetAdvanced__postcode} placeholder="Postcode" />
-      <Select className={cs.searchFieldsetAdvanced__distance} items={distances} placeholder="Alle afstanden..." />
+      <Select
+        className={cs.searchFieldsetAdvanced__distance}
+        options={distances}
+        onChange={selectedOption => {
+          setDistance(selectedOption.value)
+        }}
+        placeholder="Alle afstanden..."
+      />
     </div>
   )
 }
