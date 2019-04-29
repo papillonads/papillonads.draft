046675ab2719bc2224d5a2709374bb039a02ec58
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-29 15:52:59 +0200

    remove empty Query style reference

--- packages/client/src/pattern/molecule/header/Query/Query.jsx
+++ packages/client/src/pattern/molecule/header/Query/Query.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
-import cs from './Query.scss'
 
 const Query = ({ className }) => {
-  const queryClassList = cn(className, cs.query)
+  const queryClassList = cn(className)
 
   return <div className={queryClassList}>Query</div>
 }
