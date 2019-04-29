78dfc1ccfd782a9eefa378806ef11c7da91cd55b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-29 15:55:35 +0200

    remove empty NotFoundPage style reference

--- packages/client/src/pattern/page/NotFoundPage/NotFoundPage.jsx
+++ packages/client/src/pattern/page/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { string, object } from 'prop-types'
 import cn from 'classnames'
-import cs from './NotFoundPage.scss'
 import { Query } from 'react-apollo'
 import { PAGES } from '../../../graphql/query'
 import NotFoundTemplate from '../../template/NotFoundTemplate'
 
 const NotFoundPage = ({ className, location }) => {
-  const notFoundPageClassList = cn(className, cs.notFoundPage)
+  const notFoundPageClassList = cn(className)
 
   return (
     <div className={notFoundPageClassList}>
