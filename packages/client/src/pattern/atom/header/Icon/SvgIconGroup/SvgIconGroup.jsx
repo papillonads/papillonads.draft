45411ecde65da9089d8698d6dc505e9251c5d571
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-29 15:48:37 +0200

    remove empty SvgIconGroup style reference

--- packages/client/src/pattern/atom/header/Icon/SvgIconGroup/SvgIconGroup.jsx
+++ packages/client/src/pattern/atom/header/Icon/SvgIconGroup/SvgIconGroup.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { svgIconGroupShape } from '../SvgIcon.shape'
 import cn from 'classnames'
-import cs from './SvgIconGroup.scss'
 import { getSvgIconGroupPaths, getFill } from '../../../../../utility/asset'
 import uuidv1 from 'uuid/v1'
 
 const SvgIconGroup = ({ className, name, width, height, viewBox, fill, pathProps }) => {
-  const svgIconGroupClassList = cn(className, cs.svgIconGroup)
+  const svgIconGroupClassList = cn(className)
   const paths = getSvgIconGroupPaths(name)
 
   if (!(paths && paths.length > 0)) {
