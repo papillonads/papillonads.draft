06a8f16dda0db2ef15b03978caae518ad4fa58cc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-29 15:50:08 +0200

    remove empty UnderConstruction style reference

--- packages/client/src/pattern/atom/info/UnderConstruction/UnderConstruction.jsx
+++ packages/client/src/pattern/atom/info/UnderConstruction/UnderConstruction.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { string } from 'prop-types'
 import cn from 'classnames'
-import cs from './UnderConstruction.scss'
 
 const UnderConstruction = ({ className }) => {
-  const underConstructionClassList = cn(className, cs.underConstruction)
+  const underConstructionClassList = cn(className)
 
   return (
     <div className={underConstructionClassList}>
