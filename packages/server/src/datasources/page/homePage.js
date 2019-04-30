4676c2d0757f8f3edf68e38af8e35ea4e21747f4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-30 15:11:55 +0200

    extend home page data with search bar

--- packages/server/src/datasources/page/homePage.js
+++ packages/server/src/datasources/page/homePage.js
@@ -1,9 +1,19 @@
+const links = require('./header/navigationBar/links')
+const categories = require('./header/searchBar/searchFieldsetAdvanced/categories')
+const distances = require('./header/searchBar/searchFieldsetAdvanced/distances')
+
 const homePage = {
   id: 'home',
   name: 'Papillon Ads Holland',
   header: {
     navigationBar: {
-      links: [{ text: 'Help en info' }, { text: 'Voorwaarden' }, { text: 'Veilig handelen' }],
+      links,
+    },
+    searchBar: {
+      searchFieldsetAdvanced: {
+        categories,
+        distances,
+      },
     },
   },
 }
