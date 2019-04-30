bffbb3c8f5a1a042e6e670e484a7d1f1b77e57ad
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-30 23:11:17 +0200

    use server data for NavigationBar snapshot test

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.render.js
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.render.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavigationBar from './NavigationBar'
+import homePage from '../../../../../../server/src/datasources/page/homePage'
 
-const defaultRender = <NavigationBar />
+const defaultRender = <NavigationBar data={homePage.header.navigationBar} />
 
 export { defaultRender }
