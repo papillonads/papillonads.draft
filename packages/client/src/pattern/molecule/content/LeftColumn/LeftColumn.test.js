18dcc01a6ce3d9f7d5fab71b5cfd0d7239fcbc25
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 23:33:57 +0200

    remove LeftColumn

--- packages/client/src/pattern/molecule/content/LeftColumn/LeftColumn.test.js
+++
@@ -1,9 +0,0 @@
-import { defaultRender } from './LeftColumn.render'
-
-describe('<LeftColumn />', () => {
-  describe('Snaphot', () => {
-    test('must match defaultRender', () => {
-      expect(global.renderToJSON(defaultRender)).toMatchSnapshot()
-    })
-  })
-})
