fbd0df8a4b0e4fffcb35baae8aeac5938ee87108
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 20:43:35 +0200

    add content index

---
+++ packages/server/src/datasources/page/home/content/index.js
@@ -0,0 +1,11 @@
+const topBannerResources = require('./topBanner/resources')
+const marketingBannerResources = require('./marketingBanner/resources')
+const handyLinks = require('./leftColumn/handyLinks')
+const navigationBlock = require('./leftColumn/navigationBlock')
+
+module.exports = {
+  topBannerResources,
+  marketingBannerResources,
+  handyLinks,
+  navigationBlock,
+}
