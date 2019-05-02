05536b2db85bc09eb536039b3a98c88685b896d4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 17:20:42 +0200

    correct SearchBar render homePageData path

--- packages/client/src/pattern/molecule/header/SearchBar/SearchBar.render.js
+++ packages/client/src/pattern/molecule/header/SearchBar/SearchBar.render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SearchBar from './SearchBar'
-import homePageData from '../../../../../../server/src/datasources/page/homePageData'
+import homePageData from '../../../../../../server/src/datasources/page/home'
 
 const defaultRender = <SearchBar data={homePageData.header.searchBar} />
 
