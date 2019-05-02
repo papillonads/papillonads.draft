7205e567f22f9e9009b2a824a08c48d38edcfe66
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 16:23:21 +0200

    extend resource data with maxWidth and height

--- packages/server/src/datasources/page/home/content/marketingBanner/resources.js
+++ packages/server/src/datasources/page/home/content/marketingBanner/resources.js
@@ -1,9 +1,9 @@
 const marketingBannerResources = [
-  { url: 'https://s0.2mdn.net/8316070/1532385106174/index.html' },
-  { url: 'https://s.adroll.com/a/Z3C/YCN/Z3CYCNAN3ZCSRDVO6HPZ22.jpg' },
-  { url: 'https://s0.2mdn.net/6073727/1556615439531/index.html' },
-  { url: 'https://s0.2mdn.net/sadbundle/2504556644914954240/index.html' },
-  { url: 'https://s0.2mdn.net/ads/richmedia/studio/pv2/60764566/20190205052314854/index.html' },
+  { url: 'https://s0.2mdn.net/8316070/1532385106174/index.html', maxWidth: '', height: '' },
+  { url: 'https://s.adroll.com/a/Z3C/YCN/Z3CYCNAN3ZCSRDVO6HPZ22.jpg', maxWidth: '', height: '' },
+  { url: 'https://s0.2mdn.net/6073727/1556615439531/index.html', maxWidth: '', height: '' },
+  { url: 'https://s0.2mdn.net/sadbundle/2504556644914954240/index.html', maxWidth: '', height: '' },
+  { url: 'https://s0.2mdn.net/ads/richmedia/studio/pv2/60764566/20190205052314854/index.html', maxWidth: '', height: '' },
 ]
 
 module.exports = marketingBannerResources
--- packages/server/src/datasources/page/home/content/topBanner/resources.js
+++ packages/server/src/datasources/page/home/content/topBanner/resources.js
@@ -1,21 +1,31 @@
 const topBannerResources = [
-  { url: 'https://s0.2mdn.net/8054340/1535538205200/728x90.html' },
-  { url: 'https://s0.2mdn.net/4241177/1554204217310/tm_p3_970x250/index.html' },
-  { url: 'https://s0.2mdn.net/4270210/1556116689187/KPN ZAKELIJKE MARKT_Z_IPHONE_728x90/KPN ZAKELIJKE MARKT_Z_IPHONE_728x90.html' },
-  { url: 'https://s0.2mdn.net/4683306/1555335732594/index.html' },
-  { url: 'https://s0.2mdn.net/8137387/1548175163828/index.html' },
-  { url: 'https://s0.2mdn.net/4683306/1555335725656/index.html' },
+  { url: 'https://s0.2mdn.net/8054340/1535538205200/728x90.html', maxWidth: '730px', height: '10em' },
+  { url: 'https://s0.2mdn.net/4241177/1554204217310/tm_p3_970x250/index.html', maxWidth: '970px', height: '21em' },
+  {
+    url: 'https://s0.2mdn.net/4270210/1556116689187/KPN ZAKELIJKE MARKT_Z_IPHONE_728x90/KPN ZAKELIJKE MARKT_Z_IPHONE_728x90.html',
+    maxWidth: '730px',
+    height: '10em',
+  },
+  { url: 'https://s0.2mdn.net/4683306/1555335732594/index.html', maxWidth: '970px', height: '21em' },
+  { url: 'https://s0.2mdn.net/8137387/1548175163828/index.html', maxWidth: '730px', height: '10em' },
+  { url: 'https://s0.2mdn.net/4683306/1555335725656/index.html', maxWidth: '730px', height: '10em' },
   {
     url:
       'https://s0.2mdn.net/8391437/1-AZR_NA-FreeAcctDevGeneric-White-A_NLD_728x90_BAN_AZR-Direct__NL_None_Standard_SBAN_TN_Illustration_Benefit.png',
+    maxWidth: '730px',
+    height: '10em',
   },
   {
     url:
       'https://rdi.eu.criteo.com/delivery/r/afr.php?did=5cc9d92d7a1f33170ea6fd4c86849600&u=%7CP0mOTSrfzNS5aM%2FBd8KUEo%2BuWPip5nsI%2FpbPA5rR6GQ%3D%7C&c1=0s9CFkhJgOvdDKj0F_GVSruwR9MVQ1mBX18gzcmU642F6VvOIb_APmQCqGbv-2lC5wQpIfto8_avNJmIgNIlIyQM-I0cz-xelHxCOcuBOgM05j35OtDgeMEVTBzMviIfiXVsFpvLxSHZJ54lRawRgfTs_3GhTXn7uWABC_BX8KMCpbbP306CuM1ygS9Zz0XelR4ylHvima9HUDMyBLJHGB9uGY-lpfyJLoq0ib3wphNdTo75ZMI7TtH4Di_nLpwaHu_L9lj5GGA_qKh3oH2V5MGJdZW_8xNAmjYbIUJtM-v1l3dzh9CP1Ubo-ld0EVncDuCYWF992VdGRKg-FW6eWjt9QjRIpBuvWLBV6T5SqWg90TAOdh1usNlD1vMtKOk2PQPgI8PqzWE9ENBVzpwfDNQuLp7qeequ6P9lrD5YRvTjsVU6zjMHivzYlUNzkWB1',
+    maxWidth: '970px',
+    height: '21em',
   },
   {
     url:
       'https://rdi.eu.criteo.com/delivery/r/afr.php?did=5cc9d88b65ba5be408bbeedce8b2a400&u=%7CwJyBAQrCpEkOjlNiimDpoF5KegpgVjbW8ZtrI7lBBjk%3D%7C&c1=0s9CFkhJgOvdDKj0F_GVSruwR9MVQ1mBX18gzcmU642F6VvOIb_APmQCqGbv-2lC5wQpIfto8_avNJmIgNIlIyQM-I0cz-xeB-O76sQhw0B5mq_yuNDinwVQcd7KF8umOl_svqEqPVKYafipF1Xhdr1uSPTD57Tk8P2J2ajDYSl15JzX7_S6Cv765uTUrIq17IxuPLTHcjiScKoLRLXlT1W5GIY8DcneQi6ot454CZJNj6gm7ZmtQbOVCpo0t7mLFZkKjixAMNMI9Koqb9ASuDCZ0qaY-DJOPbs0hICmkSuSDtaxoYcpR40YvUCZ5nppj8iLAHvqu6Jd41-iIgG8dlp4gGC5n0Mg4MoLvn9TLD6G4znw_I88flA4Apc76mnwqnYmNEz1Peo3Zb0n_IGON4uT9JXKVColUCGb6jXKTwT1MoMno0H6A9cVRj0Lxhs3',
+    maxWidth: '970px',
+    height: '21em',
   },
 ]
 
