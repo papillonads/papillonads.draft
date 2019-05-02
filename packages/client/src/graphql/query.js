4a0f56c91c9a493a7d6e6d04cf1749851e6f1050
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-03 00:48:54 +0200

    rename fragment js to graphql and use in grapql query

--- packages/client/src/graphql/query.js
+++ packages/client/src/graphql/query.js
@@ -1,8 +1,7 @@
-/* eslint import/prefer-default-export: 0 */
 import gql from 'graphql-tag'
-import { infoFragment } from '../../../server/src/datasources/page/home/info/fragment'
-import { headerFragment } from '../../../server/src/datasources/page/home/header/fragment'
-import { contentFragment } from '../../../server/src/datasources/page/home/content/fragment'
+import { infoFragment } from '../../../server/src/datasources/page/home/info/fragment.graphql'
+import { headerFragment } from '../../../server/src/datasources/page/home/header/fragment.graphql'
+import { contentFragment } from '../../../server/src/datasources/page/home/content/fragment.graphql'
 
 export const PAGES = gql`
   query GetPages {
