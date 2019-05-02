a08ec38ed7a6ce813d39f7f53a31de2d7232bc05
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 17:20:23 +0200

    correct Links render homePageData path

--- packages/client/src/pattern/molecule/header/Links/Links.render.js
+++ packages/client/src/pattern/molecule/header/Links/Links.render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Links from './Links'
-import homePageData from '../../../../../../server/src/datasources/page/homePageData'
+import homePageData from '../../../../../../server/src/datasources/page/home'
 
 const defaultRender = <Links data={homePageData.header.navigationBar.links} />
 
