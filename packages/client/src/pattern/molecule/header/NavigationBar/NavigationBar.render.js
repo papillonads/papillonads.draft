c43a79ad2bd6fd216483059800e1a9102d0ca494
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 17:20:32 +0200

    correct NavigationBar render homePageData path

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.render.js
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavigationBar from './NavigationBar'
-import homePageData from '../../../../../../server/src/datasources/page/homePageData'
+import homePageData from '../../../../../../server/src/datasources/page/home'
 
 const defaultRender = <NavigationBar data={homePageData.header.navigationBar} />
 
