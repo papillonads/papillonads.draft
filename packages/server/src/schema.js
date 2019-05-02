c57fa22adc891dab1a4ca8cb23043d3bc0681e4a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 23:26:12 +0200

    abstract query, page and page connection schema

--- packages/server/src/schema.js
+++ packages/server/src/schema.js
@@ -1,69 +1,13 @@
 const { gql } = require('apollo-server')
 
-const typeDefs = gql`
+const queryTypeDef = gql`
   type Query {
     pages(pageSize: Int, after: String): PageConnection!
     page(id: String!): Page
   }
+`
 
-  type PageConnection {
-    cursor: String!
-    hasMore: Boolean!
-    pages: [Page]!
-  }
-
-  type Link {
-    text: String!
-  }
-
-  type Category {
-    value: String!
-    label: String!
-  }
-
-  type Distance {
-    value: String!
-    label: String!
-  }
-
-  type NavigationBar {
-    links: [Link]!
-  }
-
-  type SearchFieldsetAdvanced {
-    categories: [Category]!
-    distances: [Distance]!
-  }
-
-  type SearchBar {
-    searchFieldsetAdvanced: SearchFieldsetAdvanced!
-  }
-
-  type Header {
-    navigationBar: NavigationBar!
-    searchBar: SearchBar!
-  }
-
-  type Resource {
-    url: String!
-    maxWidth: String!
-    height: String!
-  }
-
-  type Banner {
-    resources: [Resource]!
-  }
-
-  type Content {
-    topBanner: Banner!
-    marketingBanner: Banner!
-  }
-
-  type Info {
-    id: String!
-    name: String!
-  }
-
+const pageTypeDef = gql`
   type Page {
     info: Info
     header: Header
@@ -71,4 +15,16 @@ const typeDefs = gql`
   }
 `
 
-module.exports = typeDefs
+const pageConnectionTypeDef = gql`
+  type PageConnection {
+    cursor: String!
+    hasMore: Boolean!
+    pages: [Page]!
+  }
+`
+
+module.exports = {
+  queryTypeDef,
+  pageTypeDef,
+  pageConnectionTypeDef,
+}
