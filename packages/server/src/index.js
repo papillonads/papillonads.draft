08a035f22e06000a422787d4cabcb1c823a49894
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 23:27:04 +0200

    use makeExecutableSchema to combine all type defs

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,12 +1,19 @@
 /* eslint no-console: 0 */
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
+const { ApolloServer, makeExecutableSchema } = require('apollo-server')
 const PageAPI = require('./datasources/page')
+const infoTypeDef = require('./datasources/page/home/info/schema')
+const headerTypeDef = require('./datasources/page/home/header/schema')
+const contentTypeDef = require('./datasources/page/home/content/schema')
+const { queryTypeDef, pageConnectionTypeDef, pageTypeDef } = require('./schema')
 const resolvers = require('./resolvers')
 
-const server = new ApolloServer({
-  typeDefs,
+const schema = makeExecutableSchema({
+  typeDefs: [queryTypeDef, pageTypeDef, pageConnectionTypeDef, infoTypeDef, headerTypeDef, contentTypeDef],
   resolvers,
+})
+
+const server = new ApolloServer({
+  schema,
   dataSources: () => ({
     pageAPI: new PageAPI(),
   }),
