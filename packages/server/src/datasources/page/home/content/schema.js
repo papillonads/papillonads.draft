fd6e985d639f199d8a61342b94aa2db9c9d596b7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-03 01:48:40 +0200

    extend content schema with left column

--- packages/server/src/datasources/page/home/content/schema.js
+++ packages/server/src/datasources/page/home/content/schema.js
@@ -11,8 +11,47 @@ const contentTypeDef = gql`
     resources: [Resource]!
   }
 
+  type Heading {
+    title: String!
+  }
+
+  type Group {
+    name: String!
+  }
+
+  type NavigationBlock {
+    heading: Heading!
+    groups: [Group]!
+  }
+
+  type HandyLink {
+    title: String!
+    description: String!
+  }
+
+  type HandyLinks {
+    heading: Heading!
+    links: [HandyLink]!
+  }
+
+  type Navigation {
+    highlightsLinks: HandyLinks!
+    navigationBlock: NavigationBlock!
+  }
+
+  type UsefulLinks {
+    buyersLinks: HandyLinks!
+    sellersLinks: HandyLinks!
+  }
+
+  type LeftColumn {
+    navigation: Navigation!
+    usefulLinks: UsefulLinks!
+  }
+
   type Content {
     topBanner: Banner!
+    leftColumn: LeftColumn!
     marketingBanner: Banner!
   }
 `
