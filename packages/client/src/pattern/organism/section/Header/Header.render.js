6b6325b19c61f41b8b1c226c5b35ea868bdaf53d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 17:16:55 +0200

    correct Header render homePageData path

--- packages/client/src/pattern/organism/section/Header/Header.render.js
+++ packages/client/src/pattern/organism/section/Header/Header.render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import homePageData from '../../../../../../server/src/datasources/page/homePageData'
+import homePageData from '../../../../../../server/src/datasources/page/home'
 
 const defaultRender = <Header data={homePageData.header} />
 
