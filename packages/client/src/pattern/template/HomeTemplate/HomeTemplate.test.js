514a9dc86c5db22ad72fc01c3497237707588b70
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-03 00:54:16 +0200

    spyOn Math.floor() and update snapshots

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.test.js
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.test.js
@@ -8,6 +8,19 @@ jest.mock('react-router-dom', () => {
 })
 
 describe('<HomeTemplate />', () => {
+  let mathFloorSpy
+
+  beforeEach(() => {
+    mathFloorSpy = jest.fn(() => {
+      return 0
+    })
+    jest.spyOn(Math, 'floor').mockImplementation(mathFloorSpy)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   describe('Snaphot', () => {
     test('must match defaultRender', () => {
       expect(global.renderToJSON(defaultRender)).toMatchSnapshot()
