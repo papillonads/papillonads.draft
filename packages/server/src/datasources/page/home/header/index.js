5684ee2730645fbdb27e2f3bbc103c350f0b14b7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 20:43:51 +0200

    add header index

---
+++ packages/server/src/datasources/page/home/header/index.js
@@ -0,0 +1,9 @@
+const links = require('./navigationBar/links')
+const categories = require('./searchBar/searchFieldsetAdvanced/categories')
+const distances = require('./searchBar/searchFieldsetAdvanced/distances')
+
+module.exports = {
+  links,
+  categories,
+  distances,
+}
