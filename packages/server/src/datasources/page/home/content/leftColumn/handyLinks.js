e534824dae4388323ef4ee81fc20c7226707613e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-02 20:46:35 +0200

    abstract content left column handy links data

---
+++ packages/server/src/datasources/page/home/content/leftColumn/handyLinks.js
@@ -0,0 +1,38 @@
+const handyLinks = {
+  highlights: {
+    heading: {
+      title: 'Uitgelicht',
+    },
+    links: [
+      { title: "Auto's", description: '>7500 nieuwe occasions per dag' },
+      { title: 'Vacatures', description: 'Vind de beste baan in je buurt' },
+      { title: 'Aanbiedingen', description: 'Scherpe prijzen, direct kopen' },
+      { title: 'Gelijk Oversteken', description: 'Extra zekerheid bij verzending' },
+    ],
+  },
+  buyer: {
+    heading: {
+      title: 'Voor de koper',
+    },
+    links: [
+      { title: 'Marktplaats Aanbieding', description: 'Elke dag nieuwe producten en uitjes met korting' },
+      { title: 'Zakelijke goederen', description: 'Een goede deal voor ondernemers' },
+      { title: 'Diensten en Vakmensen', description: 'Vind je vakman' },
+    ],
+  },
+  seller: {
+    heading: {
+      title: 'Voor de verkoper',
+    },
+    links: [
+      { title: 'Marktplaats Opvallen', description: 'Laat je advertentie opvallen' },
+      { title: 'Marktplaats Extra', description: 'Laat meerdere advertenties opvallen' },
+      { title: 'Diensten en Vakmensen', description: 'Jouw dienst aanbieden' },
+      { title: 'Admarkt (zakelijk)', description: 'Professioneel producten verkopen' },
+      { title: 'Marktplaats Aanbieding', description: 'Zonder verkoop geen kosten; bereik verzekerd' },
+      { title: 'PostNL', description: 'Online je verzending regelen' },
+    ],
+  },
+}
+
+module.exports = handyLinks
