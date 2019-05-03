dfe95ec7937786de4389af51857e902315180b8f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-03 14:29:50 +0200

    add border color

--- packages/client/src/pattern/organism/section/Content/Content.jsx
+++ packages/client/src/pattern/organism/section/Content/Content.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import cn from 'classnames'
+import cs from './Content.scss'
 import Banner from '../../../atom/content/Banner'
+import LeftColumn from '../../../molecule/content/LeftColumn'
 import { contentDefaultProps, contentPropTypes } from './Content.props'
 
 const Content = ({ className, data }) => {
-  const contentClassList = cn(className)
+  const contentClassList = cn(className, cs.content)
 
   return (
     <div className={contentClassList}>
       <Banner data={data.topBanner} />
+      <LeftColumn data={data.leftColumn} />
     </div>
   )
 }
