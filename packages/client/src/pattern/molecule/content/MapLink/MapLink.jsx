fa10949554366b4e1f832ffe00bd3b5c70eb6411
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-03 12:58:57 +0200

    add MapLink

---
+++ packages/client/src/pattern/molecule/content/MapLink/MapLink.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import cn from 'classnames'
+import cs from './LeftColumn.scss'
+import { mapLinkDefaultProps, mapLinkPropTypes } from './MapLink.props'
+
+const MapLink = ({ className, data }) => {
+  const mapLinkClassList = cn(className, cs.mapLink)
+
+  return <div className={mapLinkClassList} />
+}
+
+MapLink.defaultProps = mapLinkDefaultProps
+
+MapLink.propTypes = mapLinkPropTypes
+
+export default MapLink
---
+++ packages/client/src/pattern/molecule/content/MapLink/MapLink.render.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import MapLink from './MapLink'
+import homePageData from '../../../../../../server/src/data/page/home'
+
+const defaultRender = <MapLink data={homePageData.content.mapLink} />
+
+export { defaultRender }
