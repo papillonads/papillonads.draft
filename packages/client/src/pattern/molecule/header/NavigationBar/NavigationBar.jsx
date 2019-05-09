d390d97a3822fb95995c6a2bd44542ea4922bced
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:57:21 +0300

    remove unnecessary classnames from NavigationBar

--- packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.jsx
+++ packages/client/src/pattern/molecule/header/NavigationBar/NavigationBar.jsx
@@ -6,21 +6,17 @@ import Links from '../Links'
 import Menu from '../Menu'
 import { navigationBarDefaultProps, navigationBarPropTypes } from './NavigationBar.props'
 
-const NavigationBar = ({ className, data }) => {
-  const navigationBarClassList = cn(className, cs.navigationBar)
-
-  return (
-    <div className={navigationBarClassList}>
-      <div className={cs.maxWidth}>
-        <div className={cs.maxWidthLeft}>
-          <Logo className={cs.maxWidthLeftLogo} />
-          <Links className={cs.maxWidthLeftLinks} data={data.links} />
-        </div>
-        <Menu />
+const NavigationBar = ({ className, data }) => (
+  <div className={cn(className, cs.navigationBar)}>
+    <div className={cs.maxWidth}>
+      <div className={cs.maxWidthLeft}>
+        <Logo className={cs.maxWidthLeftLogo} />
+        <Links className={cs.maxWidthLeftLinks} data={data.links} />
       </div>
+      <Menu />
     </div>
-  )
-}
+  </div>
+)
 
 NavigationBar.defaultProps = navigationBarDefaultProps
 
