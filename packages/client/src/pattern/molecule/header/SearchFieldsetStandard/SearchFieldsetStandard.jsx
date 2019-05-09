c0bc47234092be44006152fe5abbf30148ead122
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:58:09 +0300

    remove unnecessary classnames from SearchFieldsetStandard

--- packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
@@ -4,15 +4,11 @@ import cs from './SearchFieldsetStandard.scss'
 import Input from '../../../atom/header/Input'
 import { searchFieldsetStandardDefaultProps, searchFieldsetStandardPropTypes } from './SearchFieldsetStandard.props'
 
-const SearchFieldsetStandard = ({ className }) => {
-  const searchFieldsetStandardClassList = cn(className, cs.searchFieldsetStandard)
-
-  return (
-    <div className={searchFieldsetStandardClassList}>
-      <Input className={cs.input} />
-    </div>
-  )
-}
+const SearchFieldsetStandard = ({ className }) => (
+  <div className={cn(className, cs.searchFieldsetStandard)}>
+    <Input className={cs.input} />
+  </div>
+)
 
 SearchFieldsetStandard.defaultProps = searchFieldsetStandardDefaultProps
 
