4da676a7d2f2038200cc3cd830a0413f64e9922a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:53:54 +0300

    remove unnecessary classnames from Logo

--- packages/client/src/pattern/atom/header/Logo/Logo.jsx
+++ packages/client/src/pattern/atom/header/Logo/Logo.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import cn from 'classnames'
 import SvgIconGroup from '../Icon/SvgIconGroup'
 import { logoDefaultProps, logoPropTypes } from './Logo.props'
 
-const Logo = ({ className }) => {
-  const logoClassList = cn(className)
-
-  return (
-    <div className={logoClassList}>
-      <SvgIconGroup
-        name="logo"
-        width={143}
-        height={26}
-        viewBox="0 0 528 96"
-        pathProps={[{ fill: '#2D3C4D' }, { fill: '#EEA566' }, { fill: '#2D3C4D' }]}
-      />
-    </div>
-  )
-}
+const Logo = ({ className }) => (
+  <div className={className}>
+    <SvgIconGroup
+      name="logo"
+      width={143}
+      height={26}
+      viewBox="0 0 528 96"
+      pathProps={[{ fill: '#2D3C4D' }, { fill: '#EEA566' }, { fill: '#2D3C4D' }]}
+    />
+  </div>
+)
 
 Logo.defaultProps = logoDefaultProps
 
