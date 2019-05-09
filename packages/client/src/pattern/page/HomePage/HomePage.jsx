f54aff0dd96d959501ef25d3a9fab282aedde0e2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:56:00 +0300

    remove unnecessary classnames from HomePage

--- packages/client/src/pattern/page/HomePage/HomePage.jsx
+++ packages/client/src/pattern/page/HomePage/HomePage.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import cn from 'classnames'
 import { Query } from 'react-apollo'
 import { PAGES } from '../../../graphql/query'
 import HomeTemplate from '../../template/HomeTemplate'
 import { homePageDefaultProps, homePagePropTypes } from './HomePage.props'
 
-const HomePage = ({ className }) => {
-  const homePageClassList = cn(className)
-
-  return (
-    <div className={homePageClassList}>
-      <Query query={PAGES}>
-        {({ data }) =>
-          data.pages && data.pages.pages ? <HomeTemplate data={data.pages.pages.find(page => page.info.id === 'home')} /> : null
-        }
-      </Query>
-    </div>
-  )
-}
+const HomePage = ({ className }) => (
+  <div className={className}>
+    <Query query={PAGES}>
+      {({ data }) =>
+        data.pages && data.pages.pages ? <HomeTemplate data={data.pages.pages.find(page => page.info.id === 'home')} /> : null
+      }
+    </Query>
+  </div>
+)
 
 HomePage.defaultProps = homePageDefaultProps
 
