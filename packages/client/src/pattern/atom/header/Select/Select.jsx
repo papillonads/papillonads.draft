99fc5adf3b366984f31541b8201f883203509766
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:54:39 +0300

    remove unnecessary classnames from Select

--- packages/client/src/pattern/atom/header/Select/Select.jsx
+++ packages/client/src/pattern/atom/header/Select/Select.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
-import cn from 'classnames'
 import uuidv1 from 'uuid/v1'
 import { selectDefaultProps, selectPropTypes } from './Select.props'
 
-const Select = ({ className, placeholder, items }) => {
-  const selectClassList = cn(className)
-
-  return (
-    <React.Fragment>
-      <select className={selectClassList}>
-        <option key={uuidv1()} value={placeholder}>
-          {placeholder}
+const Select = ({ className, placeholder, items }) => (
+  <React.Fragment>
+    <select className={className}>
+      <option key={uuidv1()} value={placeholder}>
+        {placeholder}
+      </option>
+      {items.map(item => (
+        <option key={uuidv1()} value={item.text}>
+          {item.text}
         </option>
-        {items.map(item => (
-          <option key={uuidv1()} value={item.text}>
-            {item.text}
-          </option>
-        ))}
-      </select>
-    </React.Fragment>
-  )
-}
+      ))}
+    </select>
+  </React.Fragment>
+)
 
 Select.defaultProps = selectDefaultProps
 
