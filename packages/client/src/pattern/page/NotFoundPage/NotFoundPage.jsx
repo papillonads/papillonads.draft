322db8010743cf082adad1886e083ee614c5d08f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:55:51 +0300

    remove unnecessary classnames from notFoundPage

--- packages/client/src/pattern/page/NotFoundPage/NotFoundPage.jsx
+++ packages/client/src/pattern/page/NotFoundPage/NotFoundPage.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import cn from 'classnames'
 import { Query } from 'react-apollo'
 import { PAGES } from '../../../graphql/query'
 import NotFoundTemplate from '../../template/NotFoundTemplate'
 import { notFoundPageDefaultProps, notFoundPagePropTypes } from './NotFoundPage.props'
 
-const NotFoundPage = ({ className, location }) => {
-  const notFoundPageClassList = cn(className)
-
-  return (
-    <div className={notFoundPageClassList}>
-      <Query query={PAGES}>
-        {({ data }) =>
-          data.pages && data.pages.pages ? (
-            <NotFoundTemplate page={data.pages.pages.find(page => page.info.id === 'not-found')} location={location} />
-          ) : null
-        }
-      </Query>
-    </div>
-  )
-}
+const NotFoundPage = ({ className, location }) => (
+  <div className={className}>
+    <Query query={PAGES}>
+      {({ data }) =>
+        data.pages && data.pages.pages ? (
+          <NotFoundTemplate page={data.pages.pages.find(page => page.info.id === 'not-found')} location={location} />
+        ) : null
+      }
+    </Query>
+  </div>
+)
 
 NotFoundPage.defaultProps = notFoundPageDefaultProps
 
