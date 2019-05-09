5339c7fe6d7e6d4b2627a551dab01ab385e42afc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:57:42 +0300

    remove unnecessary classnames from Content

--- packages/client/src/pattern/organism/section/Content/Content.jsx
+++ packages/client/src/pattern/organism/section/Content/Content.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import cn from 'classnames'
 import cs from './Content.scss'
 import Banner from '../../../atom/content/Banner'
 import LeftColumn from '../../../molecule/content/LeftColumn'
 import { contentDefaultProps, contentPropTypes } from './Content.props'
 
-const Content = ({ className, data }) => {
-  const contentClassList = cn(className)
-
-  return (
-    <div className={contentClassList}>
-      <div className={cs.maxWidth}>
-        <Banner className={cs.topBanner} data={data.topBanner} />
-        <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
-      </div>
+const Content = ({ className, data }) => (
+  <div className={className}>
+    <div className={cs.maxWidth}>
+      <Banner className={cs.topBanner} data={data.topBanner} />
+      <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
     </div>
-  )
-}
+  </div>
+)
 
 Content.defaultProps = contentDefaultProps
 
