6dc95205b2b3542a569bec2cf7b3e9d2d8f95c51
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:08:55 +0300

    remove SearchFieldsetStandard specificity

--- packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
@@ -9,7 +9,7 @@ const SearchFieldsetStandard = ({ className }) => {
 
   return (
     <div className={searchFieldsetStandardClassList}>
-      <Input className={cs.searchFieldsetStandardInput} />
+      <Input className={cs.input} />
     </div>
   )
 }
