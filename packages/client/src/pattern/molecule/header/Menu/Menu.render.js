d46746427997fa6d7929b1be1e3a70fe0c861390
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 21:51:54 +0300

    update Menu,  Dropdown renders to use header navigationbar dropdown data

--- packages/client/src/pattern/molecule/header/Menu/Menu.render.js
+++ packages/client/src/pattern/molecule/header/Menu/Menu.render.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from './Menu'
+import homePageData from '../../../../../../server/src/data/page/home'
 
-const defaultRender = <Menu />
+const defaultRender = <Menu data={homePageData.header.navigationBar.dropdown} />
 
 export { defaultRender }
