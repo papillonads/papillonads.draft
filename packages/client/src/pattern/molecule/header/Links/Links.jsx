5750118e002069fa09edb8d8639e5c664bbaa7a2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-09 20:57:12 +0300

    remove unnecessary classnames from Links

--- packages/client/src/pattern/molecule/header/Links/Links.jsx
+++ packages/client/src/pattern/molecule/header/Links/Links.jsx
@@ -5,17 +5,13 @@ import Link from '../../../atom/header/Link'
 import uuidv1 from 'uuid/v1'
 import { linksDefaultProps, linksPropTypes } from './Links.props'
 
-const Links = ({ className, data }) => {
-  const linksClassList = cn(className, cs.links)
-
-  return (
-    <div className={linksClassList}>
-      {data.map(item => (
-        <Link className={cs.link} key={uuidv1()} item={item} />
-      ))}
-    </div>
-  )
-}
+const Links = ({ className, data }) => (
+  <div className={cn(className, cs.links)}>
+    {data.map(item => (
+      <Link className={cs.link} key={uuidv1()} item={item} />
+    ))}
+  </div>
+)
 
 Links.defaultProps = linksDefaultProps
 
