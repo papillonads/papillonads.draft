5c4178ac945cc855a35216263eee4403e76fc805
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-21 10:43:36 +0300

    combine styles

--- packages/client/src/pattern/organism/section/Content/Content.jsx
+++ packages/client/src/pattern/organism/section/Content/Content.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import cn from 'classnames'
 import cs from './Content.scss'
 // import Banner from '../../../atom/content/Banner'
 import LeftColumn from '../../../molecule/content/LeftColumn'
 import { contentDefaultProps, contentPropTypes } from './Content.props'
 
 const Content = ({ className, data }) => (
-  <div className={className}>
-    <div className={cs.maxWidth}>
-      {/* <Banner className={cs.topBanner} data={data.topBanner} /> */}
-      <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
-    </div>
+  <div className={cn(className, cs.maxWidth)}>
+    {/* <Banner className={cs.topBanner} data={data.topBanner} /> */}
+    <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
   </div>
 )
 
