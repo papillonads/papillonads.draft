b6765d31ac0cd17ddaa240a1da319573dd5b4127
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-05-21 10:24:33 +0300

    remove Banner from Content

--- packages/client/src/pattern/organism/section/Content/Content.jsx
+++ packages/client/src/pattern/organism/section/Content/Content.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import cs from './Content.scss'
-import Banner from '../../../atom/content/Banner'
+// import Banner from '../../../atom/content/Banner'
 import LeftColumn from '../../../molecule/content/LeftColumn'
 import { contentDefaultProps, contentPropTypes } from './Content.props'
 
 const Content = ({ className, data }) => (
   <div className={className}>
     <div className={cs.maxWidth}>
-      <Banner className={cs.topBanner} data={data.topBanner} />
+      {/* <Banner className={cs.topBanner} data={data.topBanner} /> */}
       <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
     </div>
   </div>
