19e34c19a1e670b3a7e2c37fecf445f1f5c0fef7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-17 17:49:00 +0200

    get NotFoundTemplate up to date

--- packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.render.js
+++ packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.render.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import NotFoundTemplate from './NotFoundTemplate'
+import notFoundPageData from '../../../../../server/src/data/page/notFound/index'
 
 const props = {
-  page: {
-    id: 'not-found',
-    name: 'some-name',
-  },
+  data: notFoundPageData,
   location: {
     pathname: 'some-path-name',
   },
