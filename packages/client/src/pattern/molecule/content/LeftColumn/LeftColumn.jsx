dec3915585504ed3259d27ca2cc8057b87ecb815
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:47:42 +0200

    LeftColumn classnames and named imports

--- packages/client/src/pattern/molecule/content/LeftColumn/LeftColumn.jsx
+++ packages/client/src/pattern/molecule/content/LeftColumn/LeftColumn.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import cn from 'classnames'
-import cs from './LeftColumn.scss'
+import cx from 'classnames'
+import { leftColumn } from './LeftColumn.scss'
 import { Navigation } from '../Navigation'
 import { UsefulLinks } from '../UsefulLinks'
 import { leftColumnDefaultProps, leftColumnPropTypes } from './LeftColumn.props'
 
 export const LeftColumn = ({ className, data }) => (
-  <div className={cn(className, cs.leftColumn)}>
+  <div className={cx(className, leftColumn)}>
     <Navigation data={data.navigation} />
     <UsefulLinks data={data.usefulLinks} />
   </div>
