cb5e40ade962613160bb910c29104f2fe9187345
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:50:24 +0200

    Menu classnames and named imports

--- packages/client/src/pattern/molecule/header/Menu/Menu.jsx
+++ packages/client/src/pattern/molecule/header/Menu/Menu.jsx
@@ -1,7 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react'
-import cn from 'classnames'
-import cs from './Menu.scss'
+import cx from 'classnames'
+import {
+  _menu,
+  messages,
+  messagesIcon,
+  messagesLabel,
+  notifications,
+  notificationsIcon,
+  notificationsLabel,
+  placeAd,
+  placeAdIcon,
+  placeAdButton,
+} from './Menu.scss'
 import { Badge } from '../Badge'
 import { SvgIcon } from '../../../atom/header/Icon'
 import { Label } from '../../../atom/header/Label'
@@ -10,19 +21,19 @@ import { Button } from '../../../atom/header/Button'
 import { menuDefaultProps, menuPropTypes } from './Menu.props'
 
 export const Menu = ({ className, data }) => (
-  <div className={cn(className, cs._menu)}>
-    <Badge className={cs.messages}>
-      <SvgIcon className={cs.messagesIcon} fill="grey" name="bubbles4" width={15} height={15} />
-      <Label className={cs.messagesLabel} text="Berichten" />
+  <div className={cx(className, _menu)}>
+    <Badge className={messages}>
+      <SvgIcon className={messagesIcon} fill="grey" name="bubbles4" width={15} height={15} />
+      <Label className={messagesLabel} text="Berichten" />
     </Badge>
-    <Badge className={cs.notifications}>
-      <SvgIcon className={cs.notificationsIcon} fill="grey" name="bell" width={15} height={15} />
-      <Label className={cs.notificationsLabel} text="Meldingen" />
+    <Badge className={notifications}>
+      <SvgIcon className={notificationsIcon} fill="grey" name="bell" width={15} height={15} />
+      <Label className={notificationsLabel} text="Meldingen" />
     </Badge>
     <Dropdown data={data} />
-    <Badge className={cs.placeAd}>
-      <SvgIcon className={cs.placeAdIcon} fill="white" name="pushpin" width={15} height={15} />
-      <Button className={cs.placeAdButton}>Plaats Advertentie</Button>
+    <Badge className={placeAd}>
+      <SvgIcon className={placeAdIcon} fill="white" name="pushpin" width={15} height={15} />
+      <Button className={placeAdButton}>Plaats Advertentie</Button>
     </Badge>
   </div>
 )
