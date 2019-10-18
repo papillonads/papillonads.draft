1c68b257fc032561f15bb14eb95f09417708ffcf
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:52:47 +0200

    HomeTemplate classnames and named imports

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import cn from 'classnames'
-import cs from './HomeTemplate.scss'
+import cx from 'classnames'
+import { homeTemplate, ribbonTop, ribbonBottom } from './HomeTemplate.scss'
 import { Ribbon } from '../../atom/header/Ribbon'
 import { Header } from '../../organism/section/Header'
 import { Content } from '../../organism/section/Content'
 import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.props'
 
 export const HomeTemplate = ({ className, data }) => (
-  <div className={cn(className, cs.homeTemplate)}>
-    <Ribbon className={cs.ribbonTop} />
+  <div className={cx(className, homeTemplate)}>
+    <Ribbon className={ribbonTop} />
     <Header data={data.header} />
-    <Ribbon className={cs.ribbonBottom} />
+    <Ribbon className={ribbonBottom} />
     <Content data={data.content} />
   </div>
 )
