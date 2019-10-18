735879a66e224c0ab44958edc2667306cb3181ad
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:49:36 +0200

    Dropdown classnames and named imports

--- packages/client/src/pattern/molecule/header/Dropdown/Dropdown.jsx
+++ packages/client/src/pattern/molecule/header/Dropdown/Dropdown.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import cn from 'classnames'
+import cx from 'classnames'
 import ps from '@primer/css/core/index.scss'
 import uuidv1 from 'uuid/v1'
-import cs from './Dropdown.scss'
+import { dropdown, summary, badge, svgIcon1, label, svgIcon2, list, listItem } from './Dropdown.scss'
 import { Label } from '../../../atom/header/Label'
 import { SvgIcon } from '../../../atom/header/Icon'
 import { Badge } from '../Badge'
 import { dropdownDefaultProps, dropdownPropTypes } from './Dropdown.props'
 
 export const Dropdown = ({ className, data }) => (
-  <details className={cn(className, ps['details-reset'], cs.dropdown)}>
-    <summary aria-haspopup="true" className={cs.summary}>
-      <Badge className={cs.badge}>
-        <SvgIcon className={cs.svgIcon1} fill="grey" name="user" width={20} height={20} />
-        <Label className={cs.label} text="t" />
-        <SvgIcon className={cs.svgIcon2} fill="grey" name="ctrl" width={15} height={15} />
+  <details className={cx(className, ps['details-reset'], dropdown)}>
+    <summary aria-haspopup="true" className={summary}>
+      <Badge className={badge}>
+        <SvgIcon className={svgIcon1} fill="grey" name="user" width={20} height={20} />
+        <Label className={label} text="t" />
+        <SvgIcon className={svgIcon2} fill="grey" name="ctrl" width={15} height={15} />
       </Badge>
     </summary>
 
-    <ul className={cn(ps['dropdown-menu'], cs.list)}>
+    <ul className={cx(ps['dropdown-menu'], list)}>
       {data.map(item => (
         <li key={uuidv1()}>
-          <Label className={cn(ps['dropdown-item'], cs.listItem)} key={uuidv1()} text={item.text} />
+          <Label className={cx(ps['dropdown-item'], listItem)} key={uuidv1()} text={item.text} />
         </li>
       ))}
     </ul>
