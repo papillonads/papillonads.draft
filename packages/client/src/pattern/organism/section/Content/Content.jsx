2f93969a4fb94e0cfcb38dc782af7bbfc1b298b0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:51:30 +0200

    Content classnames and named imports

--- packages/client/src/pattern/organism/section/Content/Content.jsx
+++ packages/client/src/pattern/organism/section/Content/Content.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import cn from 'classnames'
-import cs from './Content.scss'
+import cx from 'classnames'
+import { maxWidth, leftColumn } from './Content.scss'
 // import Banner from '../../../atom/content/Banner'
 import { LeftColumn } from '../../../molecule/content/LeftColumn'
 import { contentDefaultProps, contentPropTypes } from './Content.props'
 
 export const Content = ({ className, data }) => (
-  <div className={cn(className, cs.maxWidth)}>
-    {/* <Banner className={cs.topBanner} data={data.topBanner} /> */}
-    <LeftColumn className={cs.leftColumn} data={data.leftColumn} />
+  <div className={cx(className, maxWidth)}>
+    {/* <Banner className={topBanner} data={data.topBanner} /> */}
+    <LeftColumn className={leftColumn} data={data.leftColumn} />
   </div>
 )
 
