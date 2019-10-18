f0f38a4be8711bfbdac374c92555308daf64534c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:55:36 +0200

    SearchFieldsetStandard classnames and named imports

--- packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import cn from 'classnames'
-import cs from './SearchFieldsetStandard.scss'
+import cx from 'classnames'
+import { searchFieldsetStandard, input } from './SearchFieldsetStandard.scss'
 import { Input } from '../../../atom/header/Input'
 import { searchFieldsetStandardDefaultProps, searchFieldsetStandardPropTypes } from './SearchFieldsetStandard.props'
 
 export const SearchFieldsetStandard = ({ className }) => (
-  <div className={cn(className, cs.searchFieldsetStandard)}>
-    <Input className={cs.input} />
+  <div className={cx(className, searchFieldsetStandard)}>
+    <Input className={input} />
   </div>
 )
 
