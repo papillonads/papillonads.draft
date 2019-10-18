f741de5e39c3d606d6568fb5081c26f7cd9d8e6e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-18 19:51:08 +0200

    SearchBar classnames and named imports

--- packages/client/src/pattern/molecule/header/SearchBar/SearchBar.jsx
+++ packages/client/src/pattern/molecule/header/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import cn from 'classnames'
-import cs from './SearchBar.scss'
+import cx from 'classnames'
+import { searchBar, maxWidth, maxWidthSearchFieldsetStandard, maxWidthSearchFieldsetAdvanced, maxWidthButton } from './SearchBar.scss'
 import { SearchFieldsetStandard } from '../SearchFieldsetStandard'
 import { SearchFieldsetAdvanced } from '../SearchFieldsetAdvanced'
 import { Button } from '../../../atom/header/Button'
@@ -8,12 +8,12 @@ import { Button } from '../../../atom/header/Button'
 import { searchBarDefaultProps, searchBarPropTypes } from './SearchBar.props'
 
 export const SearchBar = ({ className, data }) => (
-  <div className={cn(className, cs.searchBar)}>
-    <div className={cn(cs.maxWidth)}>
-      <SearchFieldsetStandard className={cs.maxWidthSearchFieldsetStandard} />
-      <SearchFieldsetAdvanced data={data.searchFieldsetAdvanced} className={cs.maxWidthSearchFieldsetAdvanced} />
-      <Button className={cs.maxWidthButton}>Zoek</Button>
-      {/* <SearchFormOptions className={cs.maxWidthSearchFormOptions} /> */}
+  <div className={cx(className, searchBar)}>
+    <div className={cx(maxWidth)}>
+      <SearchFieldsetStandard className={maxWidthSearchFieldsetStandard} />
+      <SearchFieldsetAdvanced data={data.searchFieldsetAdvanced} className={maxWidthSearchFieldsetAdvanced} />
+      <Button className={maxWidthButton}>Zoek</Button>
+      {/* <SearchFormOptions className={maxWidthSearchFormOptions} /> */}
     </div>
   </div>
 )
