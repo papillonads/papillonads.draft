1ca68342af3fffe74f3873e57f35de32e7be8724
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-11-09 13:53:36 +0100

    module.exports.headerTypeDef

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -2,7 +2,7 @@
 const { ApolloServer, makeExecutableSchema } = require('apollo-server')
 const { PageAPI } = require('./data/page')
 const { infoTypeDef } = require('./data/page/home/info/schema')
-const headerTypeDef = require('./data/page/home/header/schema')
+const { headerTypeDef } = require('./data/page/home/header/schema')
 const contentTypeDef = require('./data/page/home/content/schema')
 const { queryTypeDef, pageConnectionTypeDef, pageTypeDef } = require('./schema')
 const { resolvers } = require('./resolvers')
