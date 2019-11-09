d07e390b31d022efad6bbe038dd901635f293701
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-11-09 11:33:33 +0100

    module.exports schema

--- packages/server/src/schema.js
+++ packages/server/src/schema.js
@@ -1,13 +1,13 @@
 const { gql } = require('apollo-server')
 
-const queryTypeDef = gql`
+module.exports.queryTypeDef = gql`
   type Query {
     pages(pageSize: Int, after: String): PageConnection!
     page(id: String!): Page
   }
 `
 
-const pageTypeDef = gql`
+module.exports.pageTypeDef = gql`
   type Page {
     info: Info
     header: Header
@@ -15,16 +15,10 @@ const pageTypeDef = gql`
   }
 `
 
-const pageConnectionTypeDef = gql`
+module.exports.pageConnectionTypeDef = gql`
   type PageConnection {
     cursor: String!
     hasMore: Boolean!
     pages: [Page]!
   }
 `
-
-module.exports = {
-  queryTypeDef,
-  pageTypeDef,
-  pageConnectionTypeDef,
-}
