29f7a7c1a5a5f09f9d470a3ec04027f4e168fe5f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-12-30 15:05:03 +0100

    change SearchFieldsetStandard from const to function

--- packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
+++ packages/client/src/pattern/molecule/header/SearchFieldsetStandard/SearchFieldsetStandard.jsx
@@ -4,11 +4,13 @@ import { searchFieldsetStandard, input } from './SearchFieldsetStandard.scss'
 import { Input } from '../../../atom/header/Input'
 import { searchFieldsetStandardDefaultProps, searchFieldsetStandardPropTypes } from './SearchFieldsetStandard.props'
 
-export const SearchFieldsetStandard = ({ className }) => (
-  <div className={cx(className, searchFieldsetStandard)}>
-    <Input className={input} />
-  </div>
-)
+export function SearchFieldsetStandard({ className }) {
+  return (
+    <div className={cx(className, searchFieldsetStandard)}>
+      <Input className={input} />
+    </div>
+  )
+}
 
 SearchFieldsetStandard.defaultProps = searchFieldsetStandardDefaultProps
 
