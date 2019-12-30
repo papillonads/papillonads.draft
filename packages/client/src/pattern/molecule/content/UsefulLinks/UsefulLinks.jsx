705b7945fcd4ff6573c5d2411106ef7965de6258
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-12-30 14:53:54 +0100

    change UsefulLinks from const to function

--- packages/client/src/pattern/molecule/content/UsefulLinks/UsefulLinks.jsx
+++ packages/client/src/pattern/molecule/content/UsefulLinks/UsefulLinks.jsx
@@ -18,38 +18,40 @@ import {
 import { SvgIconGroup } from '../../../atom/header/Icon/SvgIconGroup'
 import { usefulLinksDefaultProps, usefulLinksPropTypes } from './UsefulLinks.props'
 
-export const UsefulLinks = ({ className, data }) => (
-  <div className={className}>
-    <div className={buyersLinks}>
-      <div className={title}>{data.buyersLinks.heading.title}</div>
-      {data.buyersLinks.links.map(link => (
-        <div className={buyersLinksLink} key={uuidv1()}>
-          <div className={buyersLinksLinkIcon}>
-            <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
+export function UsefulLinks({ className, data }) {
+  return (
+    <div className={className}>
+      <div className={buyersLinks}>
+        <div className={title}>{data.buyersLinks.heading.title}</div>
+        {data.buyersLinks.links.map(link => (
+          <div className={buyersLinksLink} key={uuidv1()}>
+            <div className={buyersLinksLinkIcon}>
+              <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
+            </div>
+            <div className={buyersLinksLinkText}>
+              <div className={buyersLinksLinkTextTitle}>{link.title}</div>
+              <div className={buyersLinksLinkTextDescription}>{link.description}</div>
+            </div>
           </div>
-          <div className={buyersLinksLinkText}>
-            <div className={buyersLinksLinkTextTitle}>{link.title}</div>
-            <div className={buyersLinksLinkTextDescription}>{link.description}</div>
+        ))}
+      </div>
+      <div className={sellersLinks}>
+        <div className={title}>{data.sellersLinks.heading.title}</div>
+        {data.sellersLinks.links.map(link => (
+          <div className={sellersLinksLink} key={uuidv1()}>
+            <div className={sellersLinksLinkIcon}>
+              <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
+            </div>
+            <div className={sellersLinksLinkText}>
+              <div className={sellersLinksLinkTextTitle}>{link.title}</div>
+              <div className={sellersLinksLinkTextDescription}>{link.description}</div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-    <div className={sellersLinks}>
-      <div className={title}>{data.sellersLinks.heading.title}</div>
-      {data.sellersLinks.links.map(link => (
-        <div className={sellersLinksLink} key={uuidv1()}>
-          <div className={sellersLinksLinkIcon}>
-            <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
-          </div>
-          <div className={sellersLinksLinkText}>
-            <div className={sellersLinksLinkTextTitle}>{link.title}</div>
-            <div className={sellersLinksLinkTextDescription}>{link.description}</div>
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
-)
+  )
+}
 
 UsefulLinks.defaultProps = usefulLinksDefaultProps
 
