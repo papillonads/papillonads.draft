e174ef9eb72d9d6beedb1347a9727307a1ed54d1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-12-30 14:01:18 +0100

    change Logo from const to function

--- packages/client/src/pattern/atom/header/Logo/Logo.jsx
+++ packages/client/src/pattern/atom/header/Logo/Logo.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { SvgIconGroup } from '../Icon/SvgIconGroup'
 import { logoDefaultProps, logoPropTypes } from './Logo.props'
 
-export const Logo = ({ className }) => (
-  <div className={className}>
-    <SvgIconGroup
-      name="logo"
-      width={143}
-      height={26}
-      viewBox="0 0 528 96"
-      pathProps={[{ fill: '#2D3C4D' }, { fill: '#EEA566' }, { fill: '#2D3C4D' }]}
-    />
-  </div>
-)
+export function Logo({ className }) {
+  return (
+    <div className={className}>
+      <SvgIconGroup
+        name="logo"
+        width={143}
+        height={26}
+        viewBox="0 0 528 96"
+        pathProps={[{ fill: '#2D3C4D' }, { fill: '#EEA566' }, { fill: '#2D3C4D' }]}
+      />
+    </div>
+  )
+}
 
 Logo.defaultProps = logoDefaultProps
 
