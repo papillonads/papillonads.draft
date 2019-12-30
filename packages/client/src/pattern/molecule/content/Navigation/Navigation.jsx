e3d7f012d91ec00ee42ad2c3fdbfe71e02f3e500
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-12-30 14:46:17 +0100

    change Navigation from const to function

--- packages/client/src/pattern/molecule/content/Navigation/Navigation.jsx
+++ packages/client/src/pattern/molecule/content/Navigation/Navigation.jsx
@@ -14,32 +14,34 @@ import {
 import { SvgIconGroup } from '../../../atom/header/Icon/SvgIconGroup'
 import { navigationDefaultProps, navigationPropTypes } from './Navigation.props'
 
-export const Navigation = ({ className, data }) => (
-  <div className={className}>
-    <div className={highlightsLinks}>
-      <div className={title}>{data.highlightsLinks.heading.title}</div>
-      {data.highlightsLinks.links.map(link => (
-        <div className={highlightsLinksLink} key={uuidv1()}>
-          <div className={highlightsLinksLinkIcon}>
-            <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
+export function Navigation({ className, data }) {
+  return (
+    <div className={className}>
+      <div className={highlightsLinks}>
+        <div className={title}>{data.highlightsLinks.heading.title}</div>
+        {data.highlightsLinks.links.map(link => (
+          <div className={highlightsLinksLink} key={uuidv1()}>
+            <div className={highlightsLinksLinkIcon}>
+              <SvgIconGroup name={link.icon} width={36} height={36} viewBox="0 0 96 96" pathProps={[]} />
+            </div>
+            <div className={highlightsLinksLinkText}>
+              <div className={highlightsLinksLinkTextTitle}>{link.title}</div>
+              <div className={highlightsLinksLinkTextDescription}>{link.description}</div>
+            </div>
           </div>
-          <div className={highlightsLinksLinkText}>
-            <div className={highlightsLinksLinkTextTitle}>{link.title}</div>
-            <div className={highlightsLinksLinkTextDescription}>{link.description}</div>
+        ))}
+      </div>
+      <div className={navigationBlockLinks}>
+        <div className={title}>{data.navigationBlock.heading.title}</div>
+        {data.navigationBlock.groups.map(link => (
+          <div className={navigationBlockLinksLink} key={uuidv1()}>
+            {link.name}
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-    <div className={navigationBlockLinks}>
-      <div className={title}>{data.navigationBlock.heading.title}</div>
-      {data.navigationBlock.groups.map(link => (
-        <div className={navigationBlockLinksLink} key={uuidv1()}>
-          {link.name}
-        </div>
-      ))}
-    </div>
-  </div>
-)
+  )
+}
 
 Navigation.defaultProps = navigationDefaultProps
 
