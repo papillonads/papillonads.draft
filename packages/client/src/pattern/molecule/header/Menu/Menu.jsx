372ed3e7d258df7bbc8436d508f0156912485ee4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-12-30 13:56:53 +0100

    change Menu from const to function

--- packages/client/src/pattern/molecule/header/Menu/Menu.jsx
+++ packages/client/src/pattern/molecule/header/Menu/Menu.jsx
@@ -20,23 +20,25 @@ import { Dropdown } from '../Dropdown'
 import { Button } from '../../../atom/header/Button'
 import { menuDefaultProps, menuPropTypes } from './Menu.props'
 
-export const Menu = ({ className, data }) => (
-  <div className={cx(className, _menu)}>
-    <Badge className={messages}>
-      <SvgIcon className={messagesIcon} fill="grey" name="bubbles4" width={15} height={15} />
-      <Label className={messagesLabel} text="Berichten" />
-    </Badge>
-    <Badge className={notifications}>
-      <SvgIcon className={notificationsIcon} fill="grey" name="bell" width={15} height={15} />
-      <Label className={notificationsLabel} text="Meldingen" />
-    </Badge>
-    <Dropdown data={data} />
-    <Badge className={placeAd}>
-      <SvgIcon className={placeAdIcon} fill="white" name="pushpin" width={15} height={15} />
-      <Button className={placeAdButton}>Plaats Advertentie</Button>
-    </Badge>
-  </div>
-)
+export function Menu({ className, data }) {
+  return (
+    <div className={cx(className, _menu)}>
+      <Badge className={messages}>
+        <SvgIcon className={messagesIcon} fill="grey" name="bubbles4" width={15} height={15} />
+        <Label className={messagesLabel} text="Berichten" />
+      </Badge>
+      <Badge className={notifications}>
+        <SvgIcon className={notificationsIcon} fill="grey" name="bell" width={15} height={15} />
+        <Label className={notificationsLabel} text="Meldingen" />
+      </Badge>
+      <Dropdown data={data} />
+      <Badge className={placeAd}>
+        <SvgIcon className={placeAdIcon} fill="white" name="pushpin" width={15} height={15} />
+        <Button className={placeAdButton}>Plaats Advertentie</Button>
+      </Badge>
+    </div>
+  )
+}
 
 Menu.defaultProps = menuDefaultProps
 
