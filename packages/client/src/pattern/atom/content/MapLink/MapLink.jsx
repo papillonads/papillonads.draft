cefb82d7b5553d8a5959a92c3aaf4a29914e0785
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-09 21:39:00 +0100

    style MapLink

--- packages/client/src/pattern/atom/content/MapLink/MapLink.jsx
+++ packages/client/src/pattern/atom/content/MapLink/MapLink.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import cx from 'classnames'
-import { mapLink, mapLinkHeading, mapLinkImage } from './MapLink.scss'
+import { mapLink, mapLinkHeadingAnchor, mapLinkImage } from './MapLink.scss'
 import { mapLinkDefaultProps, mapLinkPropTypes } from './MapLink.props'
 
 export function MapLink({ className }) {
   return (
     <div className={cx(className, mapLink)}>
-      <h3 className={mapLinkHeading}>
-        <a href="kijkinuwwijk.html">Kijk in je Wijk</a>
+      <h3>
+        <a className={mapLinkHeadingAnchor} href="kijkinuwwijk.html">
+          Kijk in je Wijk
+        </a>
       </h3>
       <a href="kijkinuwwijk.html" className={mapLinkImage}>
         <img alt="" src="https://s.marktplaats.com/z/res/images/map_link.png?break" />
