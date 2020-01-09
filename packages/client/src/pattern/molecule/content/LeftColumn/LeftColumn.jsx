d3438a49da08ade5b7c705b407b1b66ad93c700d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-09 21:21:33 +0100

    extend LeftColumn with MapLink atom

--- packages/client/src/pattern/molecule/content/LeftColumn/LeftColumn.jsx
+++ packages/client/src/pattern/molecule/content/LeftColumn/LeftColumn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import cx from 'classnames'
 import { leftColumn, leftColumnNavigation, leftColumnUsefulLinks } from './LeftColumn.scss'
 import { Navigation } from '../Navigation'
+import { MapLink } from '../../../atom/content/MapLink'
 import { UsefulLinks } from '../UsefulLinks'
 import { leftColumnDefaultProps, leftColumnPropTypes } from './LeftColumn.props'
 
@@ -9,6 +10,7 @@ export function LeftColumn({ className, data }) {
   return (
     <div className={cx(className, leftColumn)}>
       <Navigation className={leftColumnNavigation} data={data.navigation} />
+      <MapLink />
       <UsefulLinks className={leftColumnUsefulLinks} data={data.usefulLinks} />
     </div>
   )
