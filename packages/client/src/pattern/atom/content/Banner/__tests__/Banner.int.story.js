0c155bb600144ad6e528e394637389e0a9db650a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-12 17:18:34 +0100

    comment out Banner stories

--- packages/client/src/pattern/atom/content/Banner/__tests__/Banner.int.story.js
+++ packages/client/src/pattern/atom/content/Banner/__tests__/Banner.int.story.js
@@ -1,17 +1,18 @@
-import { storiesOf } from '@storybook/react'
-import { topBannerRender, marketingBannerRender } from './Banner.int.render'
-import { getInfo } from '../../../../../../../../.storybook/library'
+// import { storiesOf } from '@storybook/react'
+// import { topBannerRender, marketingBannerRender } from './Banner.int.render'
+// import { getInfo } from '../../../../../../../../.storybook/library'
 
-const topBannerRenderInfo = getInfo('topBanner')
-const marketingBannerRenderInfo = getInfo('marketingBanner')
+// const topBannerRenderInfo = getInfo('topBanner')
+// const marketingBannerRenderInfo = getInfo('marketingBanner')
 
-storiesOf('Patttern/Atom/Content/Banner/TopBanner', module).add(
-  topBannerRenderInfo.text,
-  () => topBannerRender,
-  topBannerRenderInfo.parameters,
-)
-storiesOf('Patttern/Atom/Content/Banner/MarketingBanner', module).add(
-  marketingBannerRenderInfo.text,
-  () => marketingBannerRender,
-  marketingBannerRenderInfo.parameters,
-)
+// storiesOf('Patttern/Atom/Content/Banner/TopBanner', module).add(
+//   topBannerRenderInfo.text,
+//   () => topBannerRender,
+//   topBannerRenderInfo.parameters,
+// )
+
+// storiesOf('Patttern/Atom/Content/Banner/MarketingBanner', module).add(
+//   marketingBannerRenderInfo.text,
+//   () => marketingBannerRender,
+//   marketingBannerRenderInfo.parameters,
+// )
